cf70a3cc0ea1a54b5046b1551291a6fac0740ebf
Michiel Leyman <michiel.leyman@gmail.com>
2020-05-21 13:25:53 +0200

    Add husky and prettify/lint pre-commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,19 @@
-import resolve from "@rollup/plugin-node-resolve";
-import replace from "@rollup/plugin-replace";
-import commonjs from "@rollup/plugin-commonjs";
-import svelte from "rollup-plugin-svelte";
-import babel from "rollup-plugin-babel";
-import { terser } from "rollup-plugin-terser";
-import config from "sapper/config/rollup.js";
-import autoPreprocess from "svelte-preprocess";
-import pkg from "./package.json";
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import svelte from 'rollup-plugin-svelte';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import config from 'sapper/config/rollup.js';
+import autoPreprocess from 'svelte-preprocess';
+import pkg from './package.json';
 
 const mode = process.env.NODE_ENV;
-const dev = mode === "development";
+const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-  (warning.code === "CIRCULAR_DEPENDENCY" &&
-    /[/\\]@sapper[/\\]/.test(warning.message)) ||
+  (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
   onwarn(warning);
 
 export default {
@@ -23,52 +22,52 @@ export default {
     output: config.client.output(),
     plugins: [
       replace({
-        "process.browser": true,
-        "process.env.NODE_ENV": JSON.stringify(mode),
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode)
       }),
       svelte({
         preprocess: autoPreprocess(),
         dev,
         hydratable: true,
-        emitCss: true,
+        emitCss: true
       }),
       resolve({
         browser: true,
-        dedupe: ["svelte"],
+        dedupe: ['svelte']
       }),
       commonjs(),
 
       legacy &&
         babel({
-          extensions: [".js", ".mjs", ".html", ".svelte"],
+          extensions: ['.js', '.mjs', '.html', '.svelte'],
           runtimeHelpers: true,
-          exclude: ["node_modules/@babel/**"],
+          exclude: ['node_modules/@babel/**'],
           presets: [
             [
-              "@babel/preset-env",
+              '@babel/preset-env',
               {
-                targets: "> 0.25%, not dead",
-              },
-            ],
+                targets: '> 0.25%, not dead'
+              }
+            ]
           ],
           plugins: [
-            "@babel/plugin-syntax-dynamic-import",
+            '@babel/plugin-syntax-dynamic-import',
             [
-              "@babel/plugin-transform-runtime",
+              '@babel/plugin-transform-runtime',
               {
-                useESModules: true,
-              },
-            ],
-          ],
+                useESModules: true
+              }
+            ]
+          ]
         }),
 
       !dev &&
         terser({
-          module: true,
-        }),
+          module: true
+        })
     ],
 
-    onwarn,
+    onwarn
   },
 
   server: {
@@ -76,25 +75,24 @@ export default {
     output: config.server.output(),
     plugins: [
       replace({
-        "process.browser": false,
-        "process.env.NODE_ENV": JSON.stringify(mode),
+        'process.browser': false,
+        'process.env.NODE_ENV': JSON.stringify(mode)
       }),
       svelte({
         preprocess: autoPreprocess(),
-        generate: "ssr",
-        dev,
+        generate: 'ssr',
+        dev
       }),
       resolve({
-        dedupe: ["svelte"],
+        dedupe: ['svelte']
       }),
-      commonjs(),
+      commonjs()
     ],
     external: Object.keys(pkg.dependencies).concat(
-      require("module").builtinModules ||
-        Object.keys(process.binding("natives"))
+      require('module').builtinModules || Object.keys(process.binding('natives'))
     ),
 
-    onwarn,
+    onwarn
   },
 
   serviceworker: {
@@ -103,13 +101,13 @@ export default {
     plugins: [
       resolve(),
       replace({
-        "process.browser": true,
-        "process.env.NODE_ENV": JSON.stringify(mode),
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode)
       }),
       commonjs(),
-      !dev && terser(),
+      !dev && terser()
     ],
 
-    onwarn,
-  },
+    onwarn
+  }
 };
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
-const sveltePreprocess = require("svelte-preprocess");
+const sveltePreprocess = require('svelte-preprocess');
 
 module.exports = {
   preprocess: sveltePreprocess({
     // ...svelte-preprocess options
-  }),
+  })
   // ...other svelte options
 };
