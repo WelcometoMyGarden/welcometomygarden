47e04d1397d8b28a188eb83c98c54bffc9e20d50
Michiel Leyman <michiel.leyman@gmail.com>
2020-05-22 22:41:16 +0200

    Add routing

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,25 +1,6 @@
-<main>
-  <h1>Hello world!</h1>
-</main>
+<script>
+  import { Router } from '@sveltech/routify';
+  import { routes } from '@sveltech/routify/tmp/routes';
+</script>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<Router {routes} />
---
+++ client/src/pages/index.svelte
@@ -0,0 +1,25 @@
+<main>
+  <h1>Hello world!</h1>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
