bacee8e10eaac5f2a7d5434d8f271e73e3236ccc
Michiel Leyman <michiel.leyman@gmail.com>
2020-05-25 21:59:45 +0200

    Add Hamburger and NavLink components

---
+++ src/components/Nav/Hamburger.svelte
@@ -0,0 +1,76 @@
+<script>
+  export let isOpen = false;
+</script>
+
+<button on:click class:open={isOpen}>
+  <span />
+</button>
+
+<style>
+  button {
+    display: none;
+    z-index: 30;
+    border: 0;
+    cursor: pointer;
+    background: transparent;
+    outline-offset: 0.5rem;
+    outline-color: var(--color-black);
+    height: 4rem;
+    user-select: none;
+    margin-left: auto;
+  }
+
+  span {
+    background-color: var(--color-black);
+    display: block;
+    height: 0.3rem;
+    position: relative;
+    transition: background-color 200ms ease-out;
+    width: 3rem;
+  }
+
+  span:before,
+  span:after {
+    background: var(--color-black);
+    content: '';
+    display: block;
+    height: 100%;
+    position: absolute;
+    transition: all 200ms ease-out;
+    width: 100%;
+  }
+
+  span:before {
+    top: 0.8rem;
+  }
+  span:after {
+    top: -0.8rem;
+  }
+
+  .open span {
+    background: transparent;
+  }
+
+  .open span:before {
+    transform: rotate(-45deg);
+    top: 0;
+  }
+
+  .open span:after {
+    transform: rotate(45deg);
+    top: 0;
+  }
+
+  @media (max-width: 1024px) {
+    button {
+      display: block;
+    }
+  }
+
+  @media screen and (max-width: 980px) {
+    .open span:before,
+    .open span:after {
+      background: var(--color-black);
+    }
+  }
+</style>
---
+++ src/components/Nav/NavLink.svelte
@@ -0,0 +1,58 @@
+<script>
+  export let href;
+  export let isInDrawer = true;
+
+  import { isActive } from '@sveltech/routify';
+</script>
+
+<a {href} class:mobile-nav-link={isInDrawer} class:active={$isActive('/')}>
+  <slot />
+</a>
+
+<style>
+  a {
+    color: var(--color-black);
+    /* this is fine since focus styles are applied for tab indexing */
+    outline: 0;
+    transition: color 0.3s ease;
+  }
+
+  a,
+  a:visited,
+  a:active,
+  a:hover {
+    text-decoration: none;
+  }
+
+  a:after {
+    background: none repeat scroll 0 0 transparent;
+    bottom: -0.4rem;
+    content: '';
+    display: block;
+    height: 2px;
+    left: 50%;
+    position: absolute;
+    background: var(--nav-color);
+    transition: width 0.3s ease 0s, left 0.3s ease 0s;
+    width: 0;
+  }
+
+  a.active:after,
+  a:focus:after,
+  a:hover:after {
+    width: 100%;
+    left: 0;
+  }
+
+  @media (max-width: 1024px) {
+    .mobile-nav-link:after {
+      display: none;
+    }
+
+    .mobile-nav-link.active,
+    .mobile-nav-link:focus,
+    .mobile-nav-link:hover {
+      color: var(--color-light-blue);
+    }
+  }
+</style>
