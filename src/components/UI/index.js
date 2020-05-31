eccca259265ca3ba3834c1f8dbf56554ef3353f1
h <hektor.misplon@gmail.com>
2020-05-31 14:41:51 +0200

    Add "form step" component

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from './Button.svelte';
+export { default as FormStep } from './FormStep.svelte';
 export { default as Form } from './Form.svelte';
 export { default as Field } from './Field.svelte';
