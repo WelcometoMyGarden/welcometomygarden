b6f3bfad1c1298b9e2a38fdcd31b5e723f85b3fe
h <hektor.misplon@gmail.com>
2020-05-31 14:41:08 +0200

    Add required prop

--- src/components/UI/Field.svelte
+++ src/components/UI/Field.svelte
@@ -3,10 +3,11 @@
   export let help;
   export let type;
   export let placeholder;
+  export let required;
 </script>
 
 <div class="field">
-  {#if label}<label class="label">{label}</label>{/if}
+  {#if label}<label class="label">{label}{#if required}*{/if}</label>{/if}
   {#if help}<p class="help">{help}</p>{/if}
   <div class="control">
     <input class="input" type={type} placeholder={placeholder}>
