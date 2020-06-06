6878da10168fd52506ea572733d58d87df7de0a8
Hektor Misplon <hektormisplon@users.noreply.github.com>
2020-06-06 17:03:03 +0200

    Add range slider component

---
+++ src/components/UI/Slider.svelte
@@ -0,0 +1,14 @@
+<script>
+  let value = 0;
+  let min = '0';
+  let max = '10';
+</script>
+
+<input class="input" type="number" bind:value {min} {max} />
+<input class="slider" type="range" bind:value {min} {max} />
+
+<style>
+  .input {
+    padding: 1rem;
+  }
+</style>
