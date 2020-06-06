66ae1bb302049b704312f60fdc06b9c88d910498
brentguf <brent.guffens@gmail.com>
2020-06-06 14:38:38 +0200

    HTML skeleton

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -3,7 +3,72 @@
 </script>
 
 <PageContainer>
-  <section>Index</section>
+  <div>
+    <h1>Welcome to My Garden</h1>
+    <p>
+      We are a network of citizens offering free camping spots in their gardens for slow travelers
+    </p>
+    <button>Add your garden</button>
+    <button>Explore map</button>
+  </div>
+
+  <div>
+    <div>
+      <h2>Step 1: Sign up to become or find a host</h2>
+      <p>
+        Do you want to open up your garden? Please click the Add your garden button. Are you looking
+        for a camping spot? Pick your favourite garden and contact your host! Arrange a date and
+        time for your stay and off you go!
+      </p>
+    </div>
+    <div>
+      <h2>Step 2: Plan your journey</h2>
+      <p>
+        After you’ve made arrangements with your hosts, walk or bike to your camping spot. Introduce
+        yourself, pitch your tent for the night, and continue on your hiking or biking trail the
+        next day. And have a great time, of course!
+      </p>
+    </div>
+    <div>
+      <h2>Step 3: Pitch your tent and have fun!</h2>
+      <p>
+        Have a quiet night all by yourself, have fun with your friends or family or hang out with
+        your host - whatever both parties prefer. Say fond farewells and continue on your hike or
+        ride the next day!
+      </p>
+    </div>
+  </div>
+
+  <div>
+    <div>
+      <h1>All you need to know</h1>
+      <p>
+        Here are the most important things you need to know about your next adventure. Please read
+        this FAQ section thoroughly so that you know what you’re getting yourself into.
+      </p>
+    </div>
+    <div>
+      <ul>
+        <li>Who is Welcome to My Garden for?</li>
+        <li>How do I get to my camping spot?</li>
+        <li>How do I request a stay?</li>
+        <li>What about facilities?</li>
+        <li>Does Welcome to My Garden cost anything?</li>
+      </ul>
+    </div>
+  </div>
+
+  <div>
+    <h1>Our partners</h1>
+  </div>
+
+  <div>
+    <h1>Support us</h1>
+    <p>
+      Welcome to My Garden is free to use but we have to pay a couple of bills. Make a donation to
+      keep us going!
+    </p>
+  </div>
 </PageContainer>
 
 <style>
