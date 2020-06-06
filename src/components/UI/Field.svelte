73d7108d036010bf019652ec57ae36d534852dc4
Hektor Misplon <hektormisplon@users.noreply.github.com>
2020-06-06 14:47:52 +0200

    Remove bulma

--- src/components/UI/Field.svelte
+++ src/components/UI/Field.svelte
@@ -1,15 +1,31 @@
 <script>
+  import Input from './Input.svelte';
+
   export let label;
-  export let help;
   export let type;
   export let placeholder;
   export let required;
 </script>
 
 <div class="field">
-  {#if label}<label class="label">{label}{#if required}*{/if}</label>{/if}
-  {#if help}<p class="help">{help}</p>{/if}
-  <div class="control">
-    <input class="input" type={type} placeholder={placeholder}>
-  </div>
+  {#if label}
+    <label class="label">
+      {label}
+      {#if required}*{/if}
+    </label>
+  {/if}
+  <Input class="input" {type} {placeholder} {required} />
 </div>
+
+<style>
+  .field {
+    display: flex;
+    flex-direction: column;
+    margin: 0.8rem 0;
+  }
+
+  .label {
+    padding: 0.8rem 0;
+    font-weight: bold;
+  }
+</style>
