0bc3cbfd80de4c5d4fdd156fd6c48595d4ad08dd
Hektor Misplon <hektormisplon@users.noreply.github.com>
2020-06-06 17:00:48 +0200

    Add toggle component

---
+++ src/components/UI/Toggle.svelte
@@ -0,0 +1,45 @@
+<script>
+  export let checked = false;
+</script>
+
+<div class="toggle">
+  <input id="toggle" name="toggle" type="checkbox" bind:checked />
+  <label for="toggle" />
+</div>
+
+<style>
+  .toggle {
+    position: relative;
+  }
+
+  input {
+    display: none;
+  }
+
+  input:checked + label {
+    background: var(--color-green-light);
+  }
+
+  input:checked + label:before {
+    left: 50%;
+    right: 0;
+  }
+
+  label {
+    display: block;
+    cursor: pointer;
+    padding: 1.6rem;
+    border: 1px solid var(--color-gray);
+  }
+
+  label:before {
+    content: '';
+    padding: 1.6rem;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 50%;
+    background: var(--color-gray);
+    border: 1px solid var(--color-gray);
+  }
+</style>
