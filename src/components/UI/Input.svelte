8c3d2da73e7fac1d2ff752e26d7519a2a52c4abc
Hektor Misplon <hektormisplon@users.noreply.github.com>
2020-06-06 14:50:12 +0200

    Seperate input component

---
+++ src/components/UI/Input.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let type;
+  export let name;
+  export let placeholder;
+  export let required;
+</script>
+
+<input {type} {name} {placeholder} {required} />
+
+<style>
+  input {
+    padding: 0.8rem 0;
+    border: none;
+    border-bottom: 1px solid var(--color-gray);
+    font-size: 1.6rem;
+  }
+
+  input:focus {
+    padding: 0.8rem 0;
+    border: none;
+    border-bottom: 1px solid var(--color-gray);
+    font-size: 1.6rem;
+  }
+</style>
