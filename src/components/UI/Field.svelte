a7bc6cd16ff9f794ece03d9dd786862c706d66b8
Hektor Misplon <hektormisplon@users.noreply.github.com>
2020-06-06 17:01:59 +0200

    Get field input component by type

--- src/components/UI/Field.svelte
+++ src/components/UI/Field.svelte
@@ -1,10 +1,14 @@
 <script>
   import Input from './Input.svelte';
+  import Dropdown from './Dropdown.svelte';
+  import Toggle from './Toggle.svelte';
+  import Slider from './Slider.svelte';
 
   export let label;
   export let type;
   export let placeholder;
   export let required;
+  export let options;
 </script>
 
 <div class="field">
@@ -14,7 +18,15 @@
       {#if required}*{/if}
     </label>
   {/if}
-  <Input class="input" {type} {placeholder} {required} />
+  {#if type === 'dropdown'}
+    <Dropdown {placeholder} {options} {required} />
+  {:else if type === 'toggle'}
+    <Toggle {required} />
+  {:else if type === 'slider'}
+    <Slider placeholder} {required} />
+  {:else}
+    <Input {type} {placeholder} {required} />
+  {/if}
 </div>
 
 <style>
