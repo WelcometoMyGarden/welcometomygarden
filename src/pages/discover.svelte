18843c10bee02076c3ec4d14b0c26b81082fbcd7
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-06 11:03:50 +0200

    Move map to discover pge

---
+++ src/pages/discover.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Map from '../components/Map/Map.svelte';
+</script>
+
+<Map lat="50.5" lon="4.5" zoom="7" />
+
+<style>
+
+</style>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,8 +1,8 @@
 <script>
-  import Map from '../components/Map/Map.svelte';
+
 </script>
 
-<Map lat="50.5" lon="4.5" zoom="7" />
+<h1>Index</h1>
 
 <style>
 
