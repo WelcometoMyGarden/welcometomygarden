16910a5f66fd4543e2a7d2651c7146bd2141a1c3
Hektor Misplon <hektormisplon@users.noreply.github.com>
2020-06-06 17:01:24 +0200

    Add dropdown component

---
+++ src/components/UI/Dropdown.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let selected = '';
+  export let options = [];
+</script>
+
+<select bind:value={selected}>
+  {#each options as option}
+    <option value={option}>{option}</option>
+  {/each}
+</select>
+
+<style>
+  select {
+    padding: 1rem;
+  }
+</style>
