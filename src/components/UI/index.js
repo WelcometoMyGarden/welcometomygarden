08572aaac69bda933f422c18a5ae4b8f1f966c1a
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-07 22:17:29 +0200

    Add progress to UI export

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from './Button.svelte';
 export { default as Input } from './Input.svelte';
 export { default as PageContainer } from './PageContainer.svelte';
+export { default as Progress } from './Progress.svelte';
--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -2,9 +2,8 @@
   import { onMount } from 'svelte';
   import { createAuthObserver } from '../api/auth';
   import { user, isInitializing } from '../stores/auth';
-  import PageContainer from '../components/UI/PageContainer.svelte';
+  import { PageContainer, Progress } from '../components/UI';
   import Nav from '../components/Nav/Navigation.svelte';
-  import Progress from '../components/UI/Progress.svelte';
 
   onMount(() => {
     return createAuthObserver();
