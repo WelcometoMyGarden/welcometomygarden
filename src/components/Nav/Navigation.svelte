3330b0db4b412cfc48104bc960de5647611b2bd1
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-08 14:40:45 +0200

    Add user button

--- src/components/Nav/Navigation.svelte
+++ src/components/Nav/Navigation.svelte
@@ -2,6 +2,8 @@
   import { isActive } from '@sveltech/routify';
   import routes from '@/routes';
   import NavLink from './NavLink.svelte';
+
+  const displayName = 'Marie';
 </script>
 
 <nav>
@@ -25,6 +27,12 @@
     </li>
     <li>
       <NavLink href={routes.LOGIN}>Sign in</NavLink>
+    </li>
+    <li class="user">
+      <button class="button-container user-button">
+        <div class="user-avatar">{displayName.charAt(0).toUpperCase()}</div>
+        <span>{displayName}</span>
+      </button>
     </li>
   </ul>
 </nav>
@@ -58,21 +66,60 @@
     font-size: 2.2rem;
   }
 
-  nav ul {
+  nav > ul {
     display: flex;
     align-items: center;
     box-shadow: 0 0 3.3rem rgba(0, 0, 0, 0.1);
     height: 100%;
-    padding-right: 8rem;
+    padding-right: 5rem;
   }
 
-  nav ul li {
-    margin-left: 8rem;
+  nav > ul li {
     font-weight: 600;
     position: relative;
     height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
+    min-width: 8rem;
+    text-align: center;
+    margin-left: 4rem;
+  }
+
+  .user {
+    min-width: 10rem;
+    height: 4.5rem;
+    box-shadow: 0px 0px 3.3rem rgba(0, 0, 0, 0.1);
+    font-weight: 600;
+    background-color: var(--color-white);
+    border-radius: 3rem;
+  }
+
+  .user-button {
+    border: 1px solid transparent;
+    transition: border 300ms ease-out;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 2rem;
+  }
+
+  .user-button:hover,
+  .user-button:focus,
+  .user-button:active {
+    border: 1px solid var(--color-green);
+  }
+
+  .user-avatar {
+    width: 2.8rem;
+    height: 2.8rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: var(--color-white);
+    background-color: var(--color-green-light);
+    border-radius: 50%;
+    margin-right: 1rem;
+    font-weight: bold;
   }
 </style>
