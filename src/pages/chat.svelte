a7619cc59db1bcb8860ee54958880cca7ff96b03
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-10 20:57:03 +0200

    Animate on new chat activity

--- src/pages/chat.svelte
+++ src/pages/chat.svelte
@@ -1,16 +1,19 @@
 <script>
+  import { flip } from 'svelte/animate';
   import ConversationCard from '@/components/Chat/ConversationCard.svelte';
 
-  const conversationData = {
+  let conversationData = {
     1: {
       id: 1,
       recipient: 'Dries',
-      lastMessage: 'Is your garden still available on June 3rd, because I was thinking about'
+      lastMessage: 'Is your garden still available on June 3rd, because I was thinking about',
+      lastActivity: 1
     },
     2: {
       id: 2,
       recipient: 'Manon',
-      lastMessage: 'Is your garden still available on June 3rd, because I was thinking about'
+      lastMessage: 'Is your garden still available on June 3rd, because I was thinking about',
+      lastActivity: 2
     },
     3: {
       id: 3,
@@ -44,17 +47,21 @@
     selectedConversation = conversationData[id];
   };
 
-  $: conversations = Object.keys(conversationData).map(id => conversationData[id]);
+  $: conversations = Object.keys(conversationData)
+    .map(id => conversationData[id])
+    .sort((c1, c2) => c1.lastActivity - c2.lastActivity);
 </script>
 
 <div class="container">
   <section class="conversations">
     <h2>All conversations</h2>
     {#each conversations as conversation (conversation.id)}
-      <ConversationCard
-        {...conversation}
-        selected={selectedConversation.id === conversation.id}
-        on:click={() => selectConversation(conversation.id)} />
+      <div animate:flip={{ duration: 400 }}>
+        <ConversationCard
+          {...conversation}
+          selected={selectedConversation.id === conversation.id}
+          on:click={() => selectConversation(conversation.id)} />
+      </div>
     {/each}
   </section>
   <div class="messages">
@@ -72,6 +79,8 @@
 
   .container {
     max-width: 120rem;
+    width: 100%;
+    padding: 5rem 2rem;
     display: flex;
     margin: 0 auto;
   }
@@ -81,5 +90,13 @@
     box-shadow: 0px 0px 33px rgba(0, 0, 0, 0.1);
     border-radius: 0.6rem;
     margin-right: 4rem;
+    height: 65vh;
+    overflow-y: auto;
+  }
+
+  @media screen and (max-width: 700px) {
+    .conversations {
+      height: 80vh;
+    }
   }
 </style>
