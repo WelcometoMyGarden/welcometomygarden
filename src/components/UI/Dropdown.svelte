e22c7f12dbc1cc741d210c29230747cd271a7d7e
Hektor Misplon <hektormisplon@users.noreply.github.com>
2020-06-11 12:09:29 +0200

    Validate dropdown

--- src/components/UI/Dropdown.svelte
+++ src/components/UI/Dropdown.svelte
@@ -4,9 +4,19 @@
   export let options = [];
   export let required;
   export let value = '';
+
+  let validity = null;
+
+  const handleBlur = e => (validity = e.target.validity.valid || null);
 </script>
 
-<select {name} bind:value>
+<select
+  {name}
+  bind:value
+  {required}
+  on:blur={handleBlur}
+  class:valid={validity === true}
+  class:invalid={validity === false}>
   <option value="">{placeholder}</option>
   {#each options as option}
     <option value={option}>{option}</option>
@@ -16,5 +26,13 @@
 <style>
   select {
     padding: 1rem;
+  }
+
+  .valid {
+    border: 1px solid green;
+  }
+
+  .invalid {
+    border: 1px solid red;
   }
 </style>
