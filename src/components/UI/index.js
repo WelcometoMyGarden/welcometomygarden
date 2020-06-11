83806ffe09f26a6c41303f1886fcbf3717bdd65c
Hektor Misplon <hektormisplon@users.noreply.github.com>
2020-06-11 12:10:23 +0200

    Rename "FormStep" to "Form"

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,6 +1,5 @@
 export { default as Button } from './Button.svelte';
 export { default as PageContainer } from './PageContainer.svelte';
-export { default as FormStep } from './FormStep.svelte';
 export { default as Form } from './Form.svelte';
 export { default as Fieldset } from './Fieldset.svelte';
 export { default as Field } from './Field.svelte';
