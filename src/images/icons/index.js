51516a41fefb7f146d2418e3a075d8d03a96e1aa
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-13 17:10:26 +0200

    Add icons to existing icons folder

--- src/images/icons/index.js
+++ src/images/icons/index.js
@@ -1,6 +1,11 @@
+export { default as bonfireIcon } from './bonfire.svg';
 export { default as chatIcon } from './chat.svg';
+export { default as electricityIcon } from './electricity.svg';
 export { default as mapIcon } from './map.svg';
+export { default as showerIcon } from './shower.svg';
 export { default as signInIcon } from './sign-in.svg';
 export { default as signOutIcon } from './sign-out.svg';
 export { default as tentIcon } from './tent.svg';
+export { default as toiletIcon } from './user.svg';
 export { default as userIcon } from './user.svg';
+export { default as waterIcon } from './water.svg';
