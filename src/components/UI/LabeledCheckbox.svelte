a035b68fd0e233e552aa9ad34561de29c1233bd5
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-13 20:08:45 +0200

    Replace toggle by plane jane checkbox with optional icon

---
+++ src/components/UI/LabeledCheckbox.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let name;
+  export let icon = null;
+  export let label;
+  export let value;
+
+  import Icon from './Icon.svelte';
+</script>
+
+<div>
+  <input id={name} type="checkbox" {name} {value} />
+  <label for={name}>
+    {#if icon}
+      <div class="icon">
+        <Icon {icon} />
+      </div>
+    {/if}
+    {label}
+  </label>
+</div>
+
+<style>
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  label {
+    background-image: var(--icon);
+    background-position: left center;
+    background-repeat: no-repeat;
+    display: flex;
+    align-items: center;
+  }
+
+  .icon {
+    width: 2rem;
+    height: 1.8rem;
+    display: inline-block;
+    margin: 0 0.5rem 0 1rem;
+  }
+</style>
