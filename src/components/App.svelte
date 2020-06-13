a06493a391ab879017c7b97048db17f91195f59b
Kevin Tassi <kevin.tassi@gmail.com>
2020-06-13 18:19:10 +0200

    imp(style): Generate global classes for margin
    This SCSS loop will generate css classes that we can use everywhere on the app.
    
    Use case:
    
    ```html
    <div class="mb-s">Some text...</div>
    ```
    Will add a margin-bottom of small variables value
    
    *Inspire of bootstrap

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -4,3 +4,45 @@
 </script>
 
 <Router {routes} />
+
+<style type="text/scss">
+  @import '../style/variables';
+
+  @each $size in map-keys($spacers) {
+    @each $direction in 't', 'b', 'l', 'r', 'x', 'y', '' {
+      $value: map-get($spacers, $size);
+
+      @if $direction == 'x' {
+        :global(.m#{$direction}-#{$size}) {
+          margin-left: $value;
+          margin-right: $value;
+        }
+      } @else if $direction == 'y' {
+        :global(.m#{$direction}-#{$size}) {
+          margin-top: $value;
+          margin-bottom: $value;
+        }
+      } @else if $direction == 't' {
+        :global(.m#{$direction}-#{$size}) {
+          margin-top: $value;
+        }
+      } @else if $direction == 'b' {
+        :global(.m#{$direction}-#{$size}) {
+          margin-bottom: $value;
+        }
+      } @else if $direction == 'l' {
+        :global(.m#{$direction}-#{$size}) {
+          margin-left: $value;
+        }
+      } @else if $direction == 'r' {
+        :global(.m#{$direction}-#{$size}) {
+          margin-right: $value;
+        }
+      } @else if $direction == '' {
+        :global(.m-#{$size}) {
+          margin: $value;
+        }
+      }
+    }
+  }
+</style>
