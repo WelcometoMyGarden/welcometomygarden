861c554bb27fc4dcb521d22b804dbbbc233070b4
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-14 22:33:18 +0200

    Rewrite Input to only show validity on error

--- src/components/UI/Input.svelte
+++ src/components/UI/Input.svelte
@@ -1,116 +1,90 @@
 <script>
-  import { CheckIcon, XIcon } from 'svelte-feather-icons';
-
+  export let id = null;
   export let name;
   export let type;
   export let placeholder;
   export let required;
   export let value = '';
   export let error = '';
-  export let minLength, maxLength;
+  export let minLength = null;
+  export let maxLength = null;
   export let testPattern = null;
-  export let testTitle;
+  export let isValid = true;
 
-  let validity = null;
+  import { onMount } from 'svelte';
+  import { fade } from 'svelte/transition';
+  import Icon from './Icon.svelte';
+  import { crossIcon } from '@/images/icons';
 
-  const handleBlur = e => {
-    validity = e.target.validity.valid;
-    error = e.target.validationMessage;
-  };
-  const handleInput = e => {
-    value = e.target.value;
-    validity = e.target.validity.valid || null;
-    if (error) error = e.target.validationMessage;
-  };
+  let inputElement;
+  onMount(() => {
+    inputElement.type = type;
+  });
 </script>
 
-<div class="input">
+<div class="container">
   <input
+    bind:this={inputElement}
+    bind:value
     {name}
-    {type}
     {placeholder}
     {required}
+    id={id || name}
     minlength={minLength}
     maxlength={maxLength}
     pattern={testPattern}
-    title={testTitle}
-    on:blur={handleBlur}
-    on:input={handleInput}
-    class:valid={validity === true}
-    class:invalid={validity === false} />
-  <div class="icon">
-    {#if validity === true}
-      <CheckIcon size="16" />
-    {:else if validity === false}
-      <XIcon size="16" />
+    on:blur
+    on:input
+    class:invalid={!isValid} />
+  {#if !isValid}
+    <div class="icon" transition:fade>
+      <Icon icon={crossIcon} />
+    </div>
+  {/if}
+  <div class="error">
+    {#if error}
+      <p class="error-message">{error}</p>
     {/if}
   </div>
 </div>
-<div class="error">
-  {#if error}
-    <p class="error-message">{error}</p>
-  {/if}
-</div>
 
 <style>
-  .input {
-    display: flex;
+  .container {
+    position: relative;
+    width: 100%;
   }
-
-  .icon {
-    position: absolute;
-    left: 1.2rem;
-    margin: 1.2rem;
-    padding-top: 0.2rem;
-  }
-
-  :global(input.valid:focus + .icon > svg) {
-    stroke: var(--color-success);
-  }
-
-  :global(input.invalid + .icon > svg) {
-    stroke: var(--color-warning);
-  }
-
   input {
     padding: 1.2rem 0;
     border: none;
-    border-bottom: 1px solid var(--color-gray);
+    border-bottom: 1px solid var(--color-green);
+    transition: border 300ms ease-in-out;
     font-size: 1.6rem;
     outline: none;
+    width: 100%;
+    position: relative;
+  }
+  .icon {
+    position: absolute;
+    left: -2.2rem;
+    top: 1.2rem;
+    width: 2rem;
   }
 
-  input:optional {
-    font-style: italic;
+  .icon :global(svg) {
+    fill: var(--color-orange);
   }
 
-  input.valid:focus {
-    border-bottom: 1px solid var(--color-success);
-  }
-
-  input.invalid {
-    border-bottom: 1px solid var(--color-warning);
-  }
-
-  input.valid:focus,
-  input.valid:focus::placeholder {
-    color: var(--color-success);
-  }
-
-  input.invalid,
-  input.invalid::placeholder {
-    color: var(--color-warning);
+  input:focus {
+    border-bottom: 1px solid var(--color-info);
   }
 
   .error {
-    display: flex;
-    min-height: 3.2rem;
+    height: 3rem;
   }
 
   .error-message {
-    font-style: italic;
     font-size: 1.4rem;
-    margin: auto 0;
-    color: var(--color-warning);
+    line-height: 3rem;
+    color: var(--color-orange);
   }
 </style>
