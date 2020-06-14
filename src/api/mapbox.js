a5e533e51adbae224b2a81171cf49c13341fa2b8
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-14 11:00:50 +0200

    Consistently parse address data

--- src/api/mapbox.js
+++ src/api/mapbox.js
@@ -6,6 +6,17 @@ const headers = {
   'Content-Type': 'application/json'
 };
 
+const parseAddressPiece = (address, piece) => {
+  if (!piece.id) return;
+  if (piece.id.includes('street')) address.street = piece.text;
+  if (piece.id.includes('address') && !address.street) address.street = piece.text;
+  if (piece.id.includes('postcode')) address.postalCode = piece.text;
+  if (piece.id.includes('place')) address.city = piece.text;
+  if (piece.id.includes('region')) address.region = piece.text;
+  if (piece.id.includes('country')) address.country = piece.text;
+  return address;
+};
+
 export const reverseGeocode = async ({ latitude, longitude }) => {
   const response = await fetch(
     `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?limit=1&access_token=${MAPBOX_ACCESS_TOKEN}`,
@@ -13,13 +24,9 @@ export const reverseGeocode = async ({ latitude, longitude }) => {
   );
   const data = await response.json();
   const addressData = data.features[0];
-  const address = {};
-  if (addressData.text) address.street = addressData.text;
-  addressData.context.forEach((piece) => {
-    if (piece.id && piece.id.includes('postcode')) address.postalCode = piece.text;
-    if (piece.id && piece.id.includes('place')) address.city = piece.text;
-    if (piece.id && piece.id.includes('region')) address.region = piece.text;
-    if (piece.id && piece.id.includes('country')) address.country = piece.text;
-  });
+
+  let address = addressData.context.reduce(parseAddressPiece, {});
+  // data includes one top-level feature
+  address = parseAddressPiece(address, addressData);
   return address;
 };
