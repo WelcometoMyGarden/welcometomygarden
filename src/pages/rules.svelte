fa407a7738edcb9b1931d4547e179aca9595f600
Hektor Misplon <hektor.misplon@gmail.com>
2020-06-16 09:26:52 +0200

    Set up rules page

---
+++ src/pages/rules.svelte
@@ -0,0 +1,58 @@
+<script>
+  import Collapsible from '../components/Collapsible.svelte';
+</script>
+
+<div class="content">
+  <h2>Rules</h2>
+  <div class="title-line-break" />
+  <p>
+    Here’s how Welcome to My Garden works. Make sure to read the rules before starting your
+    adventure! After all, this platform will only work well if we all play by the rules.
+  </p>
+  <div class="rules">
+    <Collapsible>
+      <h3 slot="title">Who is Welcome To My Garden for?</h3>
+      <p slot="content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eaque obcaecati laborum
+        rerum beatae culpa itaque voluptate corrupti repudiandae ea esse quas harum nam, sunt non
+        iure minima modi blanditiis?
+      </p>
+    </Collapsible>
+    <Collapsible>
+      <h3 slot="title">Who is Welcome To My Garden for?</h3>
+      <p slot="content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eaque obcaecati laborum
+        rerum beatae culpa itaque voluptate corrupti repudiandae ea esse quas harum nam, sunt non
+        iure minima modi blanditiis?
+      </p>
+    </Collapsible>
+  </div>
+</div>
+
+<style>
+  .content {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    max-width: 60rem;
+    margin: 0 auto;
+  }
+
+  h2 {
+    font-size: 2.4rem;
+    margin: 1.6rem 0;
+  }
+
+  .title-line-break {
+    border-bottom: 0.3rem solid var(--color-orange);
+    max-width: 12rem;
+    margin: 0.4rem 0 2.4rem 0;
+  }
+
+  .rules {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0px 0px 3.3rem rgba(0, 0, 0, 0.1);
+  }
+</style>
