49b401ba323be7f46727c8750bc37f9ad8c64e2b
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-17 18:35:10 +0200

    Add api methods for getting private info and setting notification preferences

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,26 @@
+import { get } from 'svelte/store';
 import { db } from './index';
+import { user, gettingPrivateUserProfile } from '@/stores/auth';
+import User from '@/models/User';
 
 export const getPublicUserProfile = async (uid) => {
-  const user = await db.collection('users').doc(uid).get();
-  if (!user.exists) throw new Error('This person does not have an account.');
-  return user.data();
+  const profile = await db.collection('users').doc(uid).get();
+  if (!profile.exists) throw new Error('This person does not have an account.');
+  return profile.data();
+};
+
+export const getPrivateUserProfile = async () => {
+  gettingPrivateUserProfile.set(true);
+  const profile = await db.collection('users-private').doc(get(user).id).get();
+  const updatedUser = new User(get(user));
+  updatedUser.setPrivateInformation(profile.data());
+  user.set(updatedUser);
+  gettingPrivateUserProfile.set(false);
+};
+
+export const setNotificationPreferences = async (preferenceName, preference) => {
+  await db
+    .collection('users-private')
+    .doc(get(user).id)
+    .update({ [`emailPreferences.${preferenceName}`]: preference });
 };
