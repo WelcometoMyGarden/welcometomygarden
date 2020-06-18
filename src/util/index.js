374a3b17ac3aabc62057d43f0b6b9d41eae1ed10
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-18 21:19:47 +0200

    Add get and set cookie functions

--- src/util/index.js
+++ src/util/index.js
@@ -1,2 +1,4 @@
 export { default as clickOutside } from './click-outside';
 export { default as countries } from './countries';
+export { default as getCookie } from './get-cookie';
+export { default as setCookie } from './set-cookie';
