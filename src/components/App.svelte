4a266292f60504bf718cb5d3a6101b277d420d02
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-18 20:56:10 +0200

    Determine user locale

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,6 +1,32 @@
 <script>
-  import { Router } from '@sveltech/routify';
+  import { Router, basepath } from '@sveltech/routify';
   import { routes } from '@sveltech/routify/tmp/routes';
+
+  const lang = navigator.language ? navigator.language.toLowerCase() : 'en';
+
+  const langs = {
+    en: [
+      'en',
+      'en-au',
+      'en-bz',
+      'en-ca',
+      'en-ie',
+      'en-jm',
+      'en-nz',
+      'en-ph',
+      'en-za',
+      'en-tt',
+      'en-gb',
+      'en-us',
+      'en-zw'
+    ],
+    nl: ['nl', 'nl-be'],
+    fr: ['fr', 'fr-be', 'fr-ca', 'fr-fr', 'fr-lu', 'fr-mc', 'fr-ch']
+  };
+
+  const locale = Object.keys(langs).find(code => langs[code].includes(lang)) || 'en';
+
+  console.log(locale);
 </script>
 
 <Router {routes} />
