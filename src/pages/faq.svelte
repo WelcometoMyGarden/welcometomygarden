1cc7af9e6f2ccea9f1ffd96bb6d3aba56940df37
Hektor Misplon <hektor.misplon@gmail.com>
2020-06-19 17:03:54 +0200

    Add faq page

---
+++ src/pages/faq.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { _ } from 'svelte-i18n';
+  import CollapsibleGroup from '../components/CollapsibleGroup.svelte';
+  import { getArrayFromLocale } from '@/util';
+</script>
+
+<div class="content">
+  <h2>{$_('faq.title')}</h2>
+  <div class="title-line-break" />
+  <p>{$_('faq.description')}</p>
+  <CollapsibleGroup collapsibles={getArrayFromLocale('faq.questions')} />
+</div>
+
+<style>
+
+</style>
