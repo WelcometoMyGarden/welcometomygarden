1718325160dd8209248d8adbd9fe1597601f719c
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-23 13:40:12 +0200

    Don't use slide transition if browser is Safari

---
+++ src/util/is-browser.js
@@ -0,0 +1,13 @@
+export default (b) => {
+  const browser = b.toLowerCase();
+
+  const browsers = {
+    safari:
+      /constructor/i.test(window.HTMLElement) ||
+      ((p) => p.toString() === '[object SafariRemoteNotification]')(
+        !window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification)
+      )
+  };
+
+  return browsers[browser];
+};
