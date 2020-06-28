2f9b0583ca972b961e92123574869cd4b51dcb9d
Michiel Leyman <michiel.leyman@gmail.com>
2020-06-28 14:01:18 +0200

    Add toggleable cycling layer

--- src/components/Map/WaymarkedTrails.svelte
+++ src/components/Map/WaymarkedTrails.svelte
@@ -1,6 +1,6 @@
 <script>
   export let showHiking = false;
-  export let showCycling = false;
+  export let showCycling = true;
 
   import { getContext, onMount } from 'svelte';
   import key from './mapbox-context.js';
@@ -8,19 +8,27 @@
   const { getMap } = getContext(key);
   const map = getMap();
 
+  const getVisibilityProperty = visibility => (visibility ? 'visible' : 'none');
+
   const toggleHikingVisibility = visible => {
-    map.setLayoutProperty('hiking-trails', 'visibility', visible ? 'visible' : 'none');
+    map.setLayoutProperty('hiking-trails', 'visibility', getVisibilityProperty(visible));
   };
 
-  const toggleCyclingVisibility = visible => {};
+  const toggleCyclingVisibility = visible => {
+    map.setLayoutProperty('cycling-trails', 'visibility', getVisibilityProperty(visible));
+  };
 
-  $: toggleHikingVisibility(showHiking);
-  $: toggleCyclingVisibility(showCycling);
-
+  let toggleable = false;
   onMount(() => {
     map.addSource('waymarked-hiking', {
       type: 'raster',
       tiles: ['https://tile.waymarkedtrails.org/hiking/{z}/{x}/{y}.png'],
+      tileSize: 256
+    });
+
+    map.addSource('waymarked-cycling', {
+      type: 'raster',
+      tiles: ['https://tile.waymarkedtrails.org/cycling/{z}/{x}/{y}.png'],
       tileSize: 256
     });
 
@@ -29,8 +37,21 @@
       type: 'raster',
       source: 'waymarked-hiking',
       layout: {
-        visibility: showHiking
+        visibility: getVisibilityProperty(showHiking)
       }
     });
+
+    map.addLayer({
+      id: 'cycling-trails',
+      type: 'raster',
+      source: 'waymarked-cycling',
+      layout: {
+        visibility: getVisibilityProperty(showCycling)
+      }
+    });
+    toggleable = true;
   });
+
+  $: if (toggleable) toggleHikingVisibility(showHiking);
+  $: if (toggleable) toggleCyclingVisibility(showCycling);
 </script>
