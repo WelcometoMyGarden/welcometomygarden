b0f8b9927e2c06044ada78d5380a594a35778628
Michiel Leyman <michiel.leyman@gmail.com>
2020-08-04 10:45:41 +0200

    Add helpers for firestore rule testing

---
+++ test/firebase/helpers.js
@@ -0,0 +1,75 @@
+const firebase = require('@firebase/testing');
+const fs = require('fs');
+const { expect } = require('jest');
+
+exports.setup = async (auth, data) => {
+  // Create a unique projectId for every firebase simulated app
+  const projectId = `rules-spec-${Date.now()}`;
+
+  const app = await firebase.initializeTestApp({
+    projectId,
+    auth
+  });
+
+  const db = app.firestore();
+
+  // Apply test rules so we can write documents
+  await firebase.loadFirestoreRules({
+    projectId,
+    rules: fs.readFileSync('firestore-test.rules', 'utf8')
+  });
+
+  // Write mock documents with test rules
+  if (data) {
+    for (const key in data) {
+      const ref = db.doc(key);
+      await ref.set(data[key]);
+    }
+  }
+
+  await firebase.loadFirestoreRules({
+    projectId,
+    rules: fs.readFileSync('firestore.rules', 'utf8')
+  });
+
+  // return the initialised DB for testing
+  return db;
+};
+
+exports.teardown = async () => {
+  // Delete all apps currently running in the firebase simulated environment
+  Promise.all(firebase.apps().map((app) => app.delete()));
+};
+
+expect.extend({
+  async toAllow(testPromise) {
+    let pass = false;
+    try {
+      await firebase.assertSucceeds(testPromise);
+      pass = true;
+    } catch (err) {
+      console.log(err);
+    }
+
+    return {
+      pass,
+      message: () => 'Expected Firebase operation to be allowed, but it was denied'
+    };
+  }
+});
+
+expect.extend({
+  async toDeny(testPromise) {
+    let pass = false;
+    try {
+      await firebase.assertFails(testPromise);
+      pass = true;
+    } catch (err) {
+      console.log(err);
+    }
+    return {
+      pass,
+      message: () => 'Expected Firebase operation to be denied, but it was allowed'
+    };
+  }
+});
