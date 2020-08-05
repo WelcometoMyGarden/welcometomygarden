be6c0ff414a7fde27192e1081dcd32f89cea5121
Ismaila Abdoulahi <code.ismaila@gmail.com>
2020-08-05 13:57:37 +0200

    Replace objects by arrays

--- src/pages/terms/cookies.svelte
+++ src/pages/terms/cookies.svelte
@@ -1,6 +1,8 @@
 <script>
   import { _, locale } from 'svelte-i18n';
   import { getArrayFromLocale, supportEmailLinkString } from '@/util';
+
+  $: articles = getArrayFromLocale('cookies.articles');
 </script>
 
 <svelte:head>
@@ -8,33 +10,11 @@
 </svelte:head>
 
 <h2>{$_('cookies.title')}</h2>
-<h3>{$_('cookies.articles.1.title')}</h3>
-<p>
-  {@html $_('cookies.articles.1.copy')}
-</p>
-<h3>{$_('cookies.articles.2.title')}</h3>
-<p>{$_('cookies.articles.2.copy')}</p>
-
-<h3>{$_('cookies.articles.3.title')}</h3>
-<p>
-  {@html $_('cookies.articles.3.copy')}
-</p>
-<h3>{$_('cookies.articles.4.title')}</h3>
-<p>{$_(`cookies.articles.4.copy.p1`)}</p>
-<p>{$_(`cookies.articles.4.copy.p2`)}</p>
-<p>{$_(`cookies.articles.4.copy.p3`)}</p>
-<p>{$_(`cookies.articles.4.copy.p4`)}</p>
-<p>{$_(`cookies.articles.4.copy.p5`)}</p>
-
-<h3>{$_('cookies.articles.5.title')}</h3>
-<p>{$_(`cookies.articles.5.copy.p1`)}</p>
-<p>{$_(`cookies.articles.5.copy.p2`)}</p>
-<p>
-  {@html $_(`cookies.articles.5.copy.p3`)}
-</p>
-<p>{$_(`cookies.articles.5.copy.p4`)}</p>
-
-<h3>{$_('cookies.articles.6.title')}</h3>
-<p>
-  {@html $_('cookies.articles.6.copy', { values: { support: supportEmailLinkString } })}
-</p>
+{#each articles as { title, copy }, i}
+  <h3>{title}</h3>
+  {#each getArrayFromLocale(`cookies.articles.${i}.copy`) as copy, j}
+    <p>
+      {@html $_(`cookies.articles.${i}.copy.${j}`, { values: { support: supportEmailLinkString } })}
+    </p>
+  {/each}
+{/each}
