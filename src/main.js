3a9bbad0e0d23107d938ac4bd57f7adc067778d1
Michiel Leyman <michiel.leyman@gmail.com>
2020-08-05 14:18:42 +0200

    Instantiate service worker from scripts, not index.html

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
+import HMR from '@sveltech/routify/hmr';
 import App from './components/App.svelte';
 
-const app = new App({ target: document.body });
+const app = HMR(App, { target: document.body }, 'routify-app');
 
 export default app;
+
+if ('serviceWorker' in navigator) {
+  import('workbox-window').then(async ({ Workbox }) => {
+    const wb = new Workbox('/service-worker.js');
+    await wb.register();
+    wb.addEventListener('installed', () => console.log('installed service worker'));
+    wb.addEventListener('externalinstalled', () => console.log('installed service worker'));
+  });
+}
