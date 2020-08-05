f9a4fcd3e46510abc5fa13d8cab410d06a2ad0f6
Ward Beyens <45149049+wardbeyens@users.noreply.github.com>
2020-08-05 18:09:48 +0200

    i18n map - underline attribution gone

--- src/pages/explore/_layout.svelte
+++ src/pages/explore/_layout.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { _ } from 'svelte-i18n';
   import { onMount, onDestroy } from 'svelte';
   import { goto, params } from '@sveltech/routify';
   import { getAllListedGardens } from '@/api/garden';
@@ -19,7 +20,7 @@
 
   let carNoticeShown = !getCookie('car-notice-dismissed');
 
-  const selectGarden = garden => {
+  const selectGarden = (garden) => {
     const newSelectedId = garden.id;
     const newGarden = $allGardens[newSelectedId];
     center = [newGarden.location.longitude, newGarden.location.latitude];
@@ -55,6 +56,8 @@
 
   let showHiking = false;
   let showCycling = false;
+
+  const attributionLinkTrails = `<a href="https://waymarkedtrails.org/" target="_blank">Waymarked Trails</a>`;
 </script>
 
 <Progress active={$isFetchingGardens} />
@@ -62,7 +65,7 @@
   <Map lat={center[1]} lon={center[0]} recenterOnUpdate zoom="7">
     {#if !$isFetchingGardens}
       <GardenLayer
-        on:garden-click={e => selectGarden(e.detail)}
+        on:garden-click={(e) => selectGarden(e.detail)}
         selectedGardenId={selectedGarden ? selectedGarden.id : null}
         allGardens={$allGardens} />
       <Drawer on:close={closeDrawer} garden={selectedGarden} />
@@ -79,11 +82,8 @@
           <div class="image-container">
             <img src="/images/no-car.svg" alt="No vehicle allowed" />
           </div>
-          <h3>Welcome To My Garden is for slow travellers only.</h3>
-          <p class="mt-m">
-            If you're planning to travel by motorized vehicle, please do not contact hosts via this
-            platform. Thank you for understanding!
-          </p>
+          <h3>{$_('map.vehicle-notice.title')}</h3>
+          <p class="mt-m">{$_('map.vehicle-notice.text')}</p>
         </div>
       </div>
     {/if}
@@ -103,9 +103,12 @@
         label="Show cycling routes"
         bind:checked={showCycling} />
     </div>
-    <span class="attribution">
+<!--     <span class="attribution">
       Trails courtesy of
       <a href="https://waymarkedtrails.org/" target="_blank">Waymarked Trails</a>
+    </span> -->
+    <span class="attribution">
+      {@html $_('map.trails.attribution', { values: { link: attributionLinkTrails } })}
     </span>
   </div>
 </div>
@@ -139,7 +142,8 @@
     display: inline-block;
   }
 
-  .attribution a {
+  .attribution a,
+  #attributionLink {
     text-decoration: underline;
   }
 
