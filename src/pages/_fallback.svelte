873dc1af477b5ac51b7e6709b3bb7cad6f11e4b2
Hektor Misplon <hektor.misplon@gmail.com>
2021-04-20 11:44:08 +0200

    Add basic '404' page

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -1,1 +1,24 @@
-<p>Not found</p>
+<div>
+  <h1>404</h1>
+  <p>Not found</p>
+</div>
+
+<style>
+  div {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  h1 {
+    padding: 3.2rem;
+    font-size: 6.4rem;
+    border-bottom: 1px solid var(--color-green);
+  }
+
+  p {
+    padding: 1.6rem;
+  }
+</style>
