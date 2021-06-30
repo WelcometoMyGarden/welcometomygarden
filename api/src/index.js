d6caf7dcc594db37ea1f80ecf336b55ce1d9455a
Michiel Leyman <michiel.leyman@gmail.com>
2021-06-30 15:10:27 +0200

    feat: run backup every 6 hours

--- api/src/index.js
+++ api/src/index.js
@@ -7,15 +7,17 @@ const {
   cleanupUserOnDelete
 } = require('./auth');
 const { onChatCreate } = require('./chat');
+const { doBackup } = require('./storage');
 
 admin.initializeApp();
 
 exports.createUser = functions.https.onCall(createUser);
 exports.requestPasswordReset = functions.https.onCall(requestPasswordReset);
 exports.resendAccountVerification = functions.https.onCall(resendAccountVerification);
+exports.cleanupUserOnDelete = functions.auth.user().onDelete(cleanupUserOnDelete);
 
 exports.notifyOnChat = functions.firestore
   .document('chats/{chatId}/{messages}/{messageId}')
   .onCreate(onChatCreate);
 
-exports.cleanupUserOnDelete = functions.auth.user().onDelete(cleanupUserOnDelete);
+exports.scheduledFirestoreBackup = functions.pubsub.schedule('every 6 hours').onRun(doBackup);
