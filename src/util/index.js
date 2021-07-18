8766632e9e9bc95a0e86d363538abaf27f3e6198
Michiel Leyman <michiel.leyman@gmail.com>
2021-07-18 19:55:13 +0200

    refactor: remove redundant getArrayFromLocale util

--- src/util/index.js
+++ src/util/index.js
@@ -1,5 +1,4 @@
 export { default as countries } from './countries';
-export { default as getArrayFromLocale } from './get-array-from-locale';
 export { default as getCookie } from './get-cookie';
 export { default as isBrowser } from './is-browser';
 export { default as removeDiacritics } from './removeDiacritics';
