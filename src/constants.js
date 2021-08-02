6a016e24793eaa17251a2a80bd15d24076976edc
Ward Beyens <wardbeyens99@gmail.com>
2021-08-02 16:08:23 +0200

    add emailAsLink to constants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const SUPPORT_EMAIL = 'support@welcometomygarden.org';
+export const emailAsLink = `<a class="link" href="mailto:${SUPPORT_EMAIL}">${SUPPORT_EMAIL}</a>`;
 export const SHOP_URL = 'https://shop.welcometomygarden.org/';
