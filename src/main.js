c82c70f3b9cefd8c946b1f97abaff69f78c80050
Michiel Leyman <michiel.leyman@gmail.com>
2021-08-11 17:08:37 +0200

    build: move styles to src folder

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import HMR from '@roxi/routify/hmr';
 import App from './components/App.svelte';
 
+import './styles/reset.css';
+import './styles/global.css';
+
 const app = HMR(App, { target: document.body }, 'app-container');
 
 export default app;
