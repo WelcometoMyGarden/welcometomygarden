14e8c581f501e8d5e49416afccb7770e2551abfc
Michiel Leyman <michiel.leyman@gmail.com>
2021-08-11 19:15:15 +0200

    fix: temporarily unregister service worker

--- src/main.js
+++ src/main.js
@@ -7,12 +7,3 @@ import './styles/global.css';
 const app = HMR(App, { target: document.body }, 'app-container');
 
 export default app;
-
-if ('serviceWorker' in navigator) {
-  import('workbox-window').then(async ({ Workbox }) => {
-    const wb = new Workbox('./service-worker.js');
-    await wb.register();
-    wb.addEventListener('installed', () => console.log('installed service worker'));
-    wb.addEventListener('externalinstalled', () => console.log('installed service worker'));
-  });
-}
