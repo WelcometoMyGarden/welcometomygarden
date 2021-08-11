38dbafc8500e820fe0ca27180e82f371b71b2f2d
Michiel Leyman <michiel.leyman@gmail.com>
2021-08-11 17:39:48 +0200

    build: load config keys correctly

--- src/api/index.js
+++ src/api/index.js
@@ -4,20 +4,18 @@ import 'firebase/functions';
 import 'firebase/storage';
 import 'firebase/auth';
 
-import { config } from '@/config';
-
 const firebaseConfig = {
-  apiKey: config.VITE_FIREBASE_API_KEY,
-  authDomain: config.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: config.VITE_FIREBASE_DATABASE_URL,
-  projectId: config.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: config.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: config.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: config.VITE_FIREBASE_APP_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-if (config.VITE_FIREBASE_MEASUREMENT_ID)
-  firebaseConfig.measurementId = config.VITE_FIREBASE_MEASUREMENT_ID;
+if (import.meta.env.VITE_FIREBASE_MEASUREMENT_ID)
+  firebaseConfig.measurementId = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
 
 const firebase = fire.initializeApp(firebaseConfig);
 
@@ -30,7 +28,7 @@ export const Timestamp = fire.firestore.Timestamp;
 
 auth.useDeviceLanguage();
 
-if (window.location.hostname === 'localhost' && config.VITE_USE_API_EMULATOR) {
+if (window.location.hostname === 'localhost' && import.meta.env.VITE_USE_API_EMULATOR) {
   functions.useFunctionsEmulator('http://localhost:5001');
 }
 
@@ -41,6 +39,6 @@ const addMetrics = async () => {
   firebase.performance();
 };
 
-if (NODE_ENV === 'production') addMetrics();
+if (import.meta.env.PROD) addMetrics();
 
 export default firebase;
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+/* eslint-env node */
+export default defineConfig(() => {
+  const isProduction = process.env.MODE === 'production' || process.env.MODE === 'staging';
+  return {
+    build: {
+      polyfillDynamicImport: false,
+      cssCodeSplit: false,
+      minify: isProduction
+    },
+    optimizeDeps: { exclude: ['@roxi/routify'] },
+    resolve: {
+      dedupe: ['@roxi/routify'],
+      alias: {
+        svelte: resolve(__dirname, 'node_modules/svelte'),
+        '@': resolve(__dirname, './src')
+      }
+    },
+    plugins: [
+      svelte({
+        hot: !isProduction,
+        emitCss: true,
+        extensions: ['.svelte']
+      })
+    ]
+  };
+});
