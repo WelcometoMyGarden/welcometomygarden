ccae3808197d6993391cabc938dbd13508c40530
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-08-24 16:02:14 +0200

    Add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,17 @@
 import * as helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Welcome To My Garden API Docs')
+    .setVersion('0.0.1')
+    .build();
+  SwaggerModule.setup('/', app, SwaggerModule.createDocument(app, config));
+
   app.use(helmet());
   await app.listen(5000);
 }
