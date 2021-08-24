8d7c96d3c131b85458b1cf34583663b55a32ce60
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-08-24 15:02:12 +0200

    Add helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
+import * as helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   await app.listen(5000);
 }
 bootstrap();
