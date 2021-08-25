7f96e774744f0296634228f0ef89c6c85e6e2030
Ward Beyens <wardbeyens99@gmail.com>
2021-08-25 16:09:27 +0200

    add donation url constant

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 export const SUPPORT_EMAIL = 'support@welcometomygarden.org';
 export const SHOP_URL = 'https://shop.welcometomygarden.org/';
+export const DONATION_URL = 'https://opencollective.com/welcometomygarden/donate';
 export const ZOOM_LEVELS = {
   CITY: 11,
   ROAD: 14, // 14 or 15
