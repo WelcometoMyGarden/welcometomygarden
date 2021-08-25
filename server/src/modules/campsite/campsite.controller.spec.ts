8f5872d9293faed366334212d48489612108ba30
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-08-25 17:42:55 +0200

    Test CampsiteController getOne and getAll

--- server/src/modules/campsite/campsite.controller.spec.ts
+++ server/src/modules/campsite/campsite.controller.spec.ts
@@ -1,22 +1,55 @@
-import { PrismaModule } from '../../config/prisma/prisma.module';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CampsiteService } from './campsite.service';
 import { CampsiteController } from './campsite.controller';
+import { CampsiteModule } from './campsite.module';
+import { Prisma } from '@prisma/client';
+
+const campsites = [
+  { id: 1, userId: 1, facilities: {} },
+  { id: 2, userId: 2, facilities: {} },
+];
 
 describe('CampsiteController', () => {
   let controller: CampsiteController;
 
+  const catsService = {
+    getAll: () => campsites,
+    getOne: (where: Prisma.CampsiteWhereUniqueInput) =>
+      campsites.find((c) => c.id === where.id),
+    createOne: (campsite: Prisma.CampsiteUpdateInput) => campsite,
+    updateOne: (params: {
+      where: Prisma.CampsiteWhereUniqueInput;
+      data: Prisma.CampsiteUpdateInput;
+    }) => ({
+      ...campsites.find((c) => c.id === params.where.id),
+      ...params.data,
+    }),
+    deleteOne: (where: Prisma.CampsiteWhereUniqueInput) => campsites[where.id],
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [PrismaModule],
-      controllers: [CampsiteController],
-      providers: [CampsiteService],
-    }).compile();
+      imports: [CampsiteModule],
+    })
+      .overrideProvider(CampsiteService)
+      .useValue(catsService)
+      .compile();
 
     controller = module.get<CampsiteController>(CampsiteController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('getAll', () => {
+    it('should return campsites', async () => {
+      expect(await controller.getAll()).toStrictEqual(catsService.getAll());
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return campsite with id = 1', async () => {
+      expect(await controller.getOne(1)).toBe(campsites[0]);
+    });
+    it('should return campsite with id = 2', async () => {
+      expect(await controller.getOne(2)).toBe(campsites[1]);
+    });
   });
 });
