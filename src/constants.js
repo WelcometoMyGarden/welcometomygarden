bcddecf652305454b05963d0422ca3e61bb13d0b
Ward Beyens <wardbeyens99@gmail.com>
2021-08-26 11:17:48 +0200

    add root url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const SUPPORT_EMAIL = 'support@welcometomygarden.org';
+export const ROOT_URL = 'https://welcometomygarden.org';
 export const SHOP_URL = 'https://shop.welcometomygarden.org/';
 export const DONATION_URL = 'https://opencollective.com/welcometomygarden/donate';
 export const ZOOM_LEVELS = {
