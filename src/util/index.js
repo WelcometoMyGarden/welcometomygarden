3faaa907d91a419c8558c3c077c20b116ecc873a
Ward Beyens <wardbeyens99@gmail.com>
2021-08-26 11:03:44 +0200

    links

--- src/util/index.js
+++ src/util/index.js
@@ -3,6 +3,6 @@ export { default as getCookie } from './get-cookie';
 export { default as isBrowser } from './is-browser';
 export { default as removeDiacritics } from './removeDiacritics';
 export { default as setCookie } from './set-cookie';
-export { emailAsLink as supportEmailLinkString } from './support-email.js';
+export { emailAsLink as supportEmailLinkString } from './links.js';
 export { default as transKeyExists } from './translation-key-exists';
 export { getNodeChildren } from './get-node-children';
