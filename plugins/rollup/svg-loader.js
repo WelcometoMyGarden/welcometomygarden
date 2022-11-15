bd0a1b848440f77ca3ff664c042cfdd7793b1ca6
Ward Beyens <wardbeyens99@gmail.com>
2022-11-15 11:14:08 +0100

    Convert to type module

--- plugins/rollup/svg-loader.js
+++ plugins/rollup/svg-loader.js
@@ -1,8 +1,9 @@
-import { getExtractedSVG } from 'svg-inline-loader';
+import pkg from 'svg-inline-loader';
+const { getExtractedSVG } = pkg;
 import fs from 'fs';
 
 //TODO: remove this once https://github.com/vitejs/vite/pull/2909 gets merged
-export default (options) => {
+export const customSvgLoader = (options) => {
   return {
     name: 'vite-svg-patch-plugin',
     transform: function (code, id) {
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import svgLoader from './plugins/rollup/svg-loader';
-import createAvailableLocales from './plugins/rollup/available-locales';
+import { customSvgLoader } from './plugins/rollup/svg-loader.js';
+import { createAvailableLocales } from './plugins/rollup/available-locales.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
 
 /* eslint-env node */
 export default defineConfig(() => {
@@ -17,13 +22,13 @@ export default defineConfig(() => {
     resolve: {
       dedupe: ['@roxi/routify'],
       alias: {
-        svelte: resolve(__dirname, 'node_modules/svelte'),
-        '@': resolve(__dirname, './src')
+        svelte: resolve(dirName, 'node_modules/svelte'),
+        '@': resolve(dirName, './src')
       }
     },
     plugins: [
       createAvailableLocales(),
-      svgLoader({ removeSVGTagAttrs: false }),
+      customSvgLoader({ removeSVGTagAttrs: false }),
       svelte({
         hot: !isProduction,
         emitCss: true,
