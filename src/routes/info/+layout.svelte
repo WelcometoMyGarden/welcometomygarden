ff085d31425f7d6b517b8ba4954a81d0f104e84f
Ward Beyens <wardbeyens99@gmail.com>
2022-11-19 23:36:51 +0200

    Add info layout

---
+++ src/routes/info/+layout.svelte
@@ -0,0 +1,46 @@
+<div class="info-page">
+  <slot />
+</div>
+
+<style>
+  .info-page {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    max-width: 90rem;
+    margin: 0 auto;
+    padding-bottom: 8rem;
+  }
+
+  .info-page > :global(.intro) {
+    margin: 3.6rem 0;
+  }
+
+  .info-page > :global(.intro > h2) {
+    font-size: 2.4rem;
+    margin: 2rem 0 3rem;
+    position: relative;
+  }
+
+  .info-page > :global(.intro > h2:after) {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: -1rem;
+    border-radius: 1rem;
+    background-color: var(--color-orange);
+    width: 12rem;
+    height: 0.3rem;
+  }
+
+  @media (max-width: 920px) {
+    .info-page > :global(.intro) {
+      padding: 0 4rem;
+      text-align: center;
+    }
+
+    .info-page > :global(.intro > h2:after) {
+      left: calc(50% - 6rem);
+    }
+  }
+</style>
