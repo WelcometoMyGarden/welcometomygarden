8986e2febf591284e644d616376da48ae21e2f33
Ward Beyens <wardbeyens99@gmail.com>
2022-11-21 11:34:37 +0200

    Add garden add page

---
+++ src/routes/garden/add/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import { _ } from 'svelte-i18n';
+  import { goto } from '$app/navigation';
+  import { addGardenLocally } from '$lib/stores/garden';
+  import { user } from '@/lib/stores/auth';
+  import notify from '$lib/stores/notification';
+  import { Progress } from '$lib/components/UI';
+  import { addGarden } from '$lib/api/garden';
+  import Form from '$lib/components/Garden/Form.svelte';
+  import routes from '$lib/routes';
+  import type { Garden } from '$lib/types/Garden';
+
+  if ($user && $user.garden) goto(routes.MANAGE_GARDEN);
+
+  let addingGarden = false;
+
+  const submit = async (e) => {
+    const garden = e.detail;
+    addingGarden = true;
+    try {
+      const newGarden = await addGarden({
+        ...garden,
+        photo: garden.photo && garden.photo.files ? garden.photo.files[0] : null
+      });
+      await addGardenLocally(newGarden);
+      addingGarden = false;
+      let notifyMsg;
+      newGarden.photo
+        ? (notifyMsg = $_('garden.notify.success') + ' ' + $_('garden.notify.photo'))
+        : (notifyMsg = $_('garden.notify.success'));
+      notify.success(notifyMsg, 10000);
+      goto(`${routes.MAP}/garden/${$user.id}`);
+    } catch (ex) {
+      console.log(ex);
+      addingGarden = false;
+    }
+  };
+
+  const initialGarden: Garden = {
+    description: '',
+    location: null,
+    facilities: {
+      capacity: 1
+    },
+    photo: {
+      files: null,
+      data: null
+    },
+    listed: true
+  };
+</script>
+
+<svelte:head>
+  <title>{$_('garden.add.title')} | Welcome To My Garden</title>
+</svelte:head>
+
+<Progress active={addingGarden} />
+
+<Form on:submit={submit} isSubmitting={addingGarden} garden={initialGarden} />
