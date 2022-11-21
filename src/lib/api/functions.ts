759ec9492418886b971c9615eb88246b28d9ac45
Ward Beyens <wardbeyens99@gmail.com>
2022-11-21 10:59:08 +0200

    Add documentation link

--- src/lib/api/functions.ts
+++ src/lib/api/functions.ts
@@ -2,6 +2,7 @@ import { functions } from './firebase';
 
 import { httpsCallable } from 'firebase/functions';
 
+// https://firebase.google.com/docs/functions/callable#call_the_function
 export const createUser = httpsCallable(functions, 'createUser');
 export const requestPasswordReset = httpsCallable(functions, 'requestPasswordReset');
 export const resendAccountVerification = httpsCallable(functions, 'resendAccountVerification');
