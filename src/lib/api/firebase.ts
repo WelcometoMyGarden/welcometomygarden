02219f68c7eac514a4ed9f0cd2b7020dd7600778
Ward Beyens <wardbeyens99@gmail.com>
2022-11-23 20:48:06 +0100

    Blocking multiple sw

--- src/lib/api/firebase.ts
+++ src/lib/api/firebase.ts
@@ -5,6 +5,7 @@ import { getApps, initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAnalytics, type Analytics } from 'firebase/analytics';
 import { getPerformance, type FirebasePerformance } from 'firebase/performance';
 import { getFunctions, type Functions } from 'firebase/functions';
+import { getMessaging, getToken, type Messaging } from "firebase/messaging";
 
 const FIREBASE_CONFIG = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
@@ -24,6 +25,7 @@ export let db: Firestore = null;
 export let auth: Auth = null;
 export let functions: Functions = null;
 export let storage: FirebaseStorage = null;
+export let messaging: Messaging = null;
 
 export let analytics: Analytics = null;
 export let performance: FirebasePerformance = null;
@@ -38,8 +40,10 @@ export async function initialize(): Promise<void> {
   auth = getAuth(app);
   storage = getStorage(app);
   functions = getFunctions(app);
+  messaging = getMessaging(app);
 
   auth.useDeviceLanguage();
+  messagingSetup();
 
   if (import.meta.env.PROD) addMetrics();
 }
@@ -48,6 +52,25 @@ const addMetrics = async () => {
   analytics = getAnalytics(app);
   performance = getPerformance(app);
 };
+
+const messagingSetup = () => {
+  getToken(messaging, { vapidKey: import.meta.env.VITE_PUBLIC_VAPID_KEY as string }).then((currentToken) => {
+    if (currentToken) {
+      console.log('Got FCM token:', currentToken);
+      // Send the token to your server and update the UI if necessary
+      // ...
+    } else {
+      // Show permission request UI
+      console.log('No registration token available. Request permission to generate one.');
+      // ...
+    }
+  }).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+  });
+}
+
+
 
 
 // TODO: Add emulator support :
---
+++ static/firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-compat.js');
+
+firebase.initializeApp( FIREBASE_CONFIG);
+
+const messaging = firebase.messaging();
+
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// Keep in mind that FCM will still show notification messages automatically
+// and you should use data messages for custom notifications.
+// For more info see:
+// https://firebase.google.com/docs/cloud-messaging/concept-options
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
