94bf73741b1f73c601b458dfef65859be8db19ca
Ward Beyens <wardbeyens99@gmail.com>
2022-11-30 14:36:13 +0100

    Add api saveGarden

--- src/lib/api/user.ts
+++ src/lib/api/user.ts
@@ -4,7 +4,11 @@ import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { db } from './firebase';
 
 import { getUser } from '@/lib/stores/auth';
-import { gettingPrivateUserProfile, updatingMailPreferences } from '@/lib/stores/user';
+import {
+  gettingPrivateUserProfile,
+  updatingMailPreferences,
+  updatingSavedGardens
+} from '@/lib/stores/user';
 import type { Garden } from '@/lib/types/Garden';
 
 export const doesPublicUserExist = async (uid: string) => {
@@ -57,4 +61,58 @@ export const updateMailPreferences = async (
 
   getUser().setEmailPreferences(preferenceName, preference);
   updatingMailPreferences.set(false);
+};
+
+const updateSavedGardens = async (gardenId: string, action: 'REMOVE' | 'ADD') => {
+  if (!gardenId || !action) throw new Error('Missing parameters');
+  if (action !== 'REMOVE' && action !== 'ADD') throw new Error('Action is not valid.');
+
+  updatingSavedGardens.set(true);
+  const docRef = doc(db(), USERS, getUser().id);
+
+  const docSnap = await getDoc(docRef);
+  if (!docSnap.exists()) throw new Error('This person does not have an account.');
+  const data = docSnap.data();
+
+  // check if user is a superfan
+  //if (!data.superfan) throw new Error('This person is not a superfan.');
+
+  // check if the user has saved gardens if not create an empty array
+  const savedGardens = data.savedGardens || [];
+
+  if (action === 'REMOVE') {
+    const index = savedGardens.indexOf(gardenId);
+    if (index > -1) {
+      // We could use a filter here but I think this is more readable and the mutation is no concern here
+      savedGardens.splice(index, 1);
+      await updateDoc(docRef, { savedGardens });
+      getUser().setAllInObject({ savedGardens });
+      updatingSavedGardens.set(false);
+      return savedGardens;
+    }
+  }
+
+  if (action === 'ADD') {
+    if (!savedGardens.includes(gardenId)) {
+      // the mutation is no concern here
+      savedGardens.push(gardenId);
+      await updateDoc(docRef, { savedGardens });
+      getUser().setAllInObject({ savedGardens });
+      updatingSavedGardens.set(false);
+      return savedGardens;
+    }
+  }
+};
+
+export const removeSavedGarden = async (gardenId: string) => {
+  await updateSavedGardens(gardenId, 'REMOVE');
+};
+export const addSavedGarden = async (gardenId: string) => {
+  await updateSavedGardens(gardenId, 'ADD');
+};
+
+export const updateSuperfan = async (superfan: boolean) => {
+  const docRef = doc(db(), USERS, getUser().id);
+  await updateDoc(docRef, { superfan });
+  getUser().superfan = superfan;
 };
