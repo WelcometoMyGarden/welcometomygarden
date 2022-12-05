47fc71723889f1dbd3f1736ea4ecff68e4ed2cd9
Ward Beyens <wardbeyens99@gmail.com>
2022-12-05 10:01:14 +0100

    Add autoprefixer

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,21 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static'; // https://www.npmjs.com/package/@sveltejs/adapter-static
-
 import preprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
+
 import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: preprocess({
+    postcss: {
+      plugins: [autoprefixer(
+        
+      )]
+    }
+  }),
 
   kit: {
     adapter: adapter({
