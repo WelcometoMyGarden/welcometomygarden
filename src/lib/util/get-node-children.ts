01a449165450aeafe95de1784d32867b02418408
Ward Beyens <wardbeyens99@gmail.com>
2022-12-19 11:33:38 +0100

    Remove old node children

--- src/lib/util/get-node-children.ts
+++ src/lib/util/get-node-children.ts
@@ -43,13 +43,3 @@ export const getNodeKeys = (nodePath: string) => {
   }
   return [];
 };
-
-export const getNodeChildrenOldVersion = (node) => {
-  const fallback = get(dictionary).en;
-  const nodes = node.split('.');
-  const nodeValue = nodes.reduce((acc, cur) => acc[cur], fallback);
-  if (nodeValue) {
-    return Object.keys(nodeValue);
-  }
-  return [];
-};
