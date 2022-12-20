5fd6a44f3e41999315070d4565fccd0e4c4902a4
Ward Beyens <wardbeyens99@gmail.com>
2022-12-20 14:43:35 +0100

    Add lang support our-story

--- src/routes/(marketing)/about-us/OurStorySection.svelte
+++ src/routes/(marketing)/about-us/OurStorySection.svelte
@@ -1,5 +1,6 @@
 <script>
   import staticAssetUrl from '$lib/util/staticAssetUrl';
+  import { _ } from 'svelte-i18n';
   import PaddedSection from '../_components/PaddedSection.svelte';
   const manonDriesInJapan = staticAssetUrl('/our-story/manon-dries-in-japan.jpg');
   const hosts = staticAssetUrl('/our-story/hosts.jpg');
@@ -8,64 +9,45 @@
 <PaddedSection backgroundColor="var(--color-beige-light)" vertical>
   <div class="wrapper">
     <img class="our-story" src={manonDriesInJapan} alt="Manon & Dries with bikes in Japan" />
-    <h2 class="our-story">Our story</h2>
+    <h2 class="our-story">{$_('our-story.title')}</h2>
     <div class="text our-story">
       <p>
-        Hey! We’re Dries and Manon and we’re the proud parents of Welcome To My Garden. In 2019, we
-        cycled from Brussels to Tokyo. Over the course of 11 months, we travelled through 21
-        countries, and were hosted by countless incredible people along the way.
+        {$_('our-story.parents')}
       </p>
       <p>
-        When we arrived back in Belgium, we asked ourselves: how can we make slow travel easier and
-        more fun in our own country? On a rainy Sunday afternoon in 2020, Dries set up the first
-        version of WTMG. Within two hours, there were 50 gardens on the map - and after two weeks,
-        there were 500!<br />We’ve come a long way since then: today, WTMG counts 30.000+ users and
-        4000+ gardens. It’s clear we’re here to stay! And it’s time to step up. We’ve listened to
-        you, our community, and we’re now investing in making WTMG meet all your slow travel needs.
+        {$_('our-story.origin')}
       </p>
       <p>
-        When we arrived back in Belgium, we asked ourselves: how can we make slow travel easier and
-        more fun in our own country? On a rainy Sunday afternoon in 2020, Dries set up the first
-        version of WTMG. Within two hours, there were 50 gardens on the map - and after two weeks,
-        there were 500!
+        {$_('our-story.long-way')}
       </p>
     </div>
-    <h2 class="bigger-picture">The bigger picture</h2>
+    <h2 class="bigger-picture">{$_('our-story.big-picture')}</h2>
     <div class="text bigger-picture">
       <p>
-        If there’s one thing we’ve learned over the past few years, it’s this: it’s time to slow
-        down. This applies to life in general, but also definitely to travel specifically. The
-        faster we travel, the more we miss out on. Fast travel generates a lot of CO2 emissions,
-        disconnects people from their surroundings, and damages communities. Enter slow travel!
+        {$_('our-story.slow-down')}
       </p>
       <p>
-        And enter WTMG! We are proud to be part of the solution to this global challenge. In two
-        years’ time, WTMG has achieved a lot:
+        {$_('our-story.proud')}
       </p>
       <ul>
-        <li><span>31.000 users have embarked on a slow travel adventure</span></li>
         <li>
-          <span>4.300 warm-hearted hosts have opened up their garden to slow travellers</span>
-        </li>
-        <li><span>57.000 stay requests have been sent through the WTMG platform</span></li>
-        <li>
-          <span>
-            We started in Belgium but more and more gardens are now also popping up across Europe
-            and beyond
-          </span>
+          <span>{$_('our-story.users')}</span>
         </li>
         <li>
-          <span>
-            We received financial support from partners like Wikimedia Belgium, Grote Routepaden
-            vzw, GR Sentiers and Natuurpunt
-          </span>
+          <span>{$_('our-story.hosts')}</span>
+        </li>
+        <li>
+          <span>{$_('our-story.stays')}</span>
+        </li>
+        <li>
+          <span>{$_('our-story.everywhere')}</span>
+        </li>
+        <li>
+          <span>{$_('our-story.financial-support')}</span>
         </li>
       </ul>
       <p>
-        The most meaningful and memorable experiences happen when we take our time to explore and
-        engage with our surroundings. With WTMG, we want to help you do just that. Together, we can
-        make the world a better place – by travelling more slowly and more sustainably. Will you
-        join the ride?
+        {$_('our-story.experiences')}
       </p>
     </div>
     <img class="bigger-picture" src={hosts} alt="Two hosts holding a WTMG sign." />
