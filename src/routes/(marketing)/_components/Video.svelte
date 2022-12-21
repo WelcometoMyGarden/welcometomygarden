09e8d3592a6da1295e5949f4d472e022e0bd90da
Thor Galle <thorgalle@gmail.com>
2022-12-21 19:43:58 +0100

    Add YouTube embed

--- src/routes/(marketing)/_components/Video.svelte
+++ src/routes/(marketing)/_components/Video.svelte
@@ -1,11 +1,15 @@
-<script>
-  import staticAssetUrl from '@/lib/util/staticAssetUrl';
-</script>
-
-<video controls src={staticAssetUrl('/Superfans_latest.mp4')} />
+<iframe
+  width="560"
+  height="315"
+  src="https://www.youtube-nocookie.com/embed/Dnuzpb-Om1M"
+  title="YouTube video player"
+  frameborder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  allowfullscreen
+/>
 
 <style>
-  video {
+  iframe {
     border-radius: 2rem;
     width: 100%;
   }
