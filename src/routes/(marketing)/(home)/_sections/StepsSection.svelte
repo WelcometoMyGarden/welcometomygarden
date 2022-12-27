604150d14f9fdc9293b1872cd044b3f3e6f90701
Thor Galle <thorgalle@gmail.com>
2022-12-27 20:10:50 +0200

    Better text & image alignment in the home steps section

--- src/routes/(marketing)/(home)/_sections/StepsSection.svelte
+++ src/routes/(marketing)/(home)/_sections/StepsSection.svelte
@@ -2,61 +2,70 @@
   import routes from '$lib/routes';
   import { _ } from 'svelte-i18n';
   import { getNodeKeys } from '@/lib/util/get-node-children';
-  import Step1 from '$lib/images/step-1.svg';
-  import Step2 from '$lib/images/step-2.svg';
-  import Step3 from '$lib/images/step-3.svg';
+  import Step1 from '$lib/images/home-steps/step-register.svg';
+  import Step2 from '$lib/images/home-steps/step-map.svg';
+  import Step3 from '$lib/images/home-steps/step-tent.svg';
   const stepGraphics = [Step1, Step2, Step3];
 </script>
 
 <section class="steps" id="steps-section">
   {#each getNodeKeys('index.steps') as key, i}
-    <div class="step">
-      <div class="step-logo">
-        {@html stepGraphics[i]}
-      </div>
-      <h2 class="oh2 step-header">
-        {$_(`index.steps.${key}.title`)}
-      </h2>
-      <p class="step-text">
-        {@html $_(`index.steps.${i}.copy`, {
-          values: {
-            addGardenLink: `<a href=${routes.ADD_GARDEN}>${$_(
-              `index.steps.${i}.add-garden-link-text`
-            )}</a>`
-          }
-        })}
-      </p>
+    <div class="step-logo">
+      {@html stepGraphics[i]}
     </div>
+    <h2 class="oh2 step-header">
+      {$_(`index.steps.${key}.title`)}
+    </h2>
+    <p class="step-text">
+      {@html $_(`index.steps.${i}.copy`, {
+        values: {
+          addGardenLink: `<a href=${routes.ADD_GARDEN}>${$_(
+            `index.steps.${i}.add-garden-link-text`
+          )}</a>`
+        }
+      })}
+    </p>
   {/each}
 </section>
 
 <style>
   .steps {
-    display: flex;
     padding: 0 10rem;
-    text-align: justify;
-    justify-content: space-between;
+    display: grid;
+    grid-template-rows: 18rem 0.8fr 2fr;
+    grid-auto-flow: column;
+    column-gap: 5%;
   }
 
-  .step {
-    display: grid;
-    grid-template-rows: 17rem 0.8fr 2fr;
-    max-width: 60rem;
-    width: 30%;
+  .steps > * {
+    /* Relative to the grid cell */
+    max-width: 40rem;
   }
 
   .step-header {
     font-family: var(--font-copy);
     font-weight: bold;
+    align-self: start;
+    margin-bottom: 2rem;
   }
 
   .step-logo {
     display: flex;
     justify-content: center;
-    align-items: center;
-    width: 60%;
-    height: 15rem;
+    align-items: flex-end;
+    padding-bottom: 2.5rem;
+    height: 100%;
     margin: 0 auto;
+  }
+
+  .step-logo > :global(svg) {
+    max-width: 95%;
+    height: min-content;
+    max-height: 70%;
+  }
+
+  .step-text {
+    align-self: start;
   }
 
   .step-text :global(a),
@@ -67,25 +76,38 @@
     text-decoration: underline;
   }
 
-  .step-logo > :global(svg) {
-    max-height: 100%;
-    max-width: 100%;
+  @media only screen and (max-width: 1100px) {
+    .step-header {
+      /* Nicer in French, where around here lines start collapsing */
+      text-align: center;
+    }
   }
 
-  @media only screen and (max-width: 1150px) {
+  @media only screen and (max-width: 1000px) {
     .steps {
-      flex-direction: column;
+      /* flex-direction: column; */
+      grid-template-columns: 100%;
+      grid-template-rows: repeat(3, 18rem auto auto);
       padding: 0 12rem;
-      align-items: center;
+      justify-items: center;
     }
 
-    .step {
-      width: 100%;
+    .steps > * {
+      /* Relative to the grid cell */
+      max-width: 47rem;
     }
 
     .step-header {
-      text-align: center;
       margin-bottom: 1rem;
+    }
+
+    .step-text {
+      text-align: center;
+    }
+
+    .step-logo :global(svg) {
+      max-height: 80%;
+      max-width: 24rem;
     }
   }
 
@@ -98,10 +120,6 @@
   @media only screen and (max-width: 700px) {
     .steps {
       padding: 0 8rem;
-    }
-
-    .step {
-      margin: 0 auto;
     }
   }
 
