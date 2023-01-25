bc0a549e07b17a811d5c2392d3c52314155ff328
Thor Galle <thorgalle@gmail.com>
2023-01-25 19:00:02 +0200

    test: add Vitest & firestore rules unit tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  testDir: './tests/e2e',
   webServer: {
     command: 'npm run build && npm run preview',
     port: 4173
