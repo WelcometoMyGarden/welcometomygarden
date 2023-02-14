5cf4b7e9198b73473f45e85c7c97120b498fa412
Thor Galle <thorgalle@gmail.com>
2023-02-14 18:11:48 +0200

    feat: link translated verification emails

--- api/src/auth.js
+++ api/src/auth.js
@@ -45,14 +45,19 @@ const {
 
 const frontendUrl = removeEndingSlash(functions.config().frontend.url);
 
-const sendVerificationEmail = async (email, firstName) => {
+/**
+ * @param {string} email
+ * @param {string} firstName
+ * @param {string} language
+ */
+const sendVerificationEmail = async (email, firstName, language) => {
   // https://firebase.google.com/docs/auth/admin/email-action-links#generate_email_verification_link
   // https://firebase.google.com/docs/auth/custom-email-handler
   const link = await auth.generateEmailVerificationLink(email, {
     url: `${frontendUrl}/account`
   });
 
-  await sendAccountVerificationEmail(email, firstName, link);
+  await sendAccountVerificationEmail(email, firstName, link, language);
 };
 
 /**
@@ -154,7 +159,7 @@ exports.createUser = async (data, context) => {
       .doc('users')
       .set({ count: FieldValue.increment(1) }, { merge: true });
 
-    await sendVerificationEmail(email, firstName);
+    await sendVerificationEmail(email, firstName, data.communicationLanguage);
 
     return { message: 'Your account was created successfully,', success: true };
   } catch (ex) {
@@ -192,23 +197,42 @@ exports.changeEmail = async () => {};
 
 exports.resendAccountVerification = async (data, context) => {
   if (!context.auth) {
-    throw new functions.https.HttpsError('unauthenticated');
+    throw new functions.https.HttpsError('unauthenticated', '');
   }
 
+  /** @type {UserRecord} */
   let user;
+  /** @type {UserPrivate} */
+  let userPrivate;
   try {
-    user = await auth.getUser(context.auth.uid);
+    const [userIn, userPrivateIn] = await Promise.all([
+      auth.getUser(context.auth.uid),
+      /** @type {Promise<UserPrivate>} */ (
+        db
+          .doc(`users-private/${context.auth.uid}`)
+          .get()
+          .then((dS) => dS.data())
+      )
+    ]);
+    user = userIn;
+    userPrivate = userPrivateIn;
   } catch (ex) {
     console.log(
       'Exception while trying to get the user while resending the account verification email',
       ex
     );
-    throw new functions.https.HttpsError('permission-denied');
+    throw new functions.https.HttpsError('permission-denied', '');
   }
 
-  if (!user || user.emailVerified) throw new functions.https.HttpsError('permission-denied');
+  if (!user || user.emailVerified) throw new functions.https.HttpsError('permission-denied', '');
+  if (!user.email) throw new functions.https.HttpsError('invalid-argument', '');
 
-  await sendVerificationEmail(user.email, user.displayName);
+  await sendVerificationEmail(
+    user.email,
+    // Empty string shouldn't occur, as we make it mandatory and set it on user creation.
+    user.displayName ?? '',
+    userPrivate.communicationLanguage ?? 'en'
+  );
 };
 
 /**
