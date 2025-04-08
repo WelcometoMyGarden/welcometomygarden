2666561ae03c51bff1b8c6cb6fe33a90ef324571
Thor Galle <thorgalle@gmail.com>
2025-04-08 17:31:43 +0200

    refactor: on email change, create the new SG contact and delete the old one at the same time, report statuses

--- api/src/sendgrid/updateContactEmail.js
+++ api/src/sendgrid/updateContactEmail.js
@@ -18,14 +18,23 @@ exports.updateSendgridContactEmail = async (firebaseUser, oldUserPrivateData) =>
     `Updating the SendGrid email of uid ${firebaseUser.uid} (contact id ${oldUserPrivateData.sendgridId}) to ${firebaseUser.email}`
   );
 
-  // Create the new contact
-  await createSendgridContact(firebaseUser.uid);
-
   // Delete the old contact (if it exists)
-  //
-  // Why do this after creating the new contact?
-  // For some reason a contact deletion causes the creation jobs to take much longer
-  // then exceed the Firebase function max timeout of 60s
-  // TODO: this would be a good candidate to give much more time (2-5 min?) in Functions v2
-  await deleteContact(firebaseUser, oldUserPrivateData);
+  const [deletedOldSendGridContact, createdNewSendGridContact] = (
+    await Promise.allSettled([
+      deleteContact(firebaseUser, oldUserPrivateData),
+      createSendgridContact(firebaseUser.uid)
+    ])
+  ).map((p) => p.status === 'fulfilled');
+  if (!deletedOldSendGridContact || !createdNewSendGridContact) {
+    logger.error(
+      `Email change did not go well:` +
+        `${!deletedOldSendGridContact ? 'old contact deletion failed' : ''}` +
+        `${!createdNewSendGridContact ? 'new contact creation failed' : ''}`,
+      {
+        uid: firebaseUser.uid,
+        new_email: firebaseUser.email,
+        old_contact_id: oldUserPrivateData.sendgridId
+      }
+    );
+  }
 };
