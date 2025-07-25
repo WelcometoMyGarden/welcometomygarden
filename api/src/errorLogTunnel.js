bcf2478aa9a56965c8d23e5477910aafedb7cfd6
Thor Galle <thorgalle@gmail.com>
2025-07-25 15:06:02 +0200

    fix: forward client IP in the error log tunnel
    To better understand how many IPs are affected by errors. Currently, the shared Google Cloud IPs are not helpful.

--- api/src/errorLogTunnel.js
+++ api/src/errorLogTunnel.js
@@ -16,8 +16,12 @@ exports.errorLogTunnel = async (req, res) => {
   }
   try {
     const envelope = req.body;
-    const firstLine = envelope.split('\n')[0];
-    const header = JSON.parse(firstLine);
+    // Example format of the envelope: line-delimited JSON, with 3 lines: header, type, data
+    // {"event_id":"uuid","sent_at":"2025-07-25T13:29:25.768Z","sdk":{...details...},"dsn":"...","trace":{"environment":"Production","release":"...","public_key":"77f31f80871092134be4b99979c9720e","trace_id":"...","transaction":"/(static)/(home)","sampled":"true","sample_rand":"0.5892881306933779","sample_rate":"1"}}
+    // {"type":"transaction"}
+    // {"contexts": ... }
+    const [headerJSON, ...envelopeRestJSONS] = envelope.split('\n');
+    const header = JSON.parse(headerJSON);
     const dsn = new URL(header['dsn']);
     const project_id = dsn.pathname?.replace('/', '');
 
@@ -26,9 +30,24 @@ exports.errorLogTunnel = async (req, res) => {
     }
 
     const upstream_sentry_url = `https://${SENTRY_HOST}/api/${project_id}/envelope/`;
+
+    // See https://forum.sentry.io/t/real-client-ip-with-sentry-nextjs-tunnel/15438/2
+    const forwardedEnvelope = [
+      // New header line
+      JSON.stringify({
+        ...header,
+        forwarded_for:
+          typeof req.headers['x-forwarded-for'] === 'string'
+            ? req.headers['x-forwarded-for']
+            : req.socket.remoteAddress
+      }),
+      // Other two lines
+      ...envelopeRestJSONS
+    ].join('\n');
+
     await fetch(upstream_sentry_url, {
       method: 'POST',
-      body: envelope,
+      body: forwardedEnvelope,
       headers: {
         'Content-Type': 'application/x-sentry-envelope'
       }
