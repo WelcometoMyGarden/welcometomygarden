8dae06734484cbf80c7864fe691cfbd39b6d27fe
Thor Galle <thorgalle@gmail.com>
2026-02-10 14:46:43 +0100

    wip: remoe safe area plugin & explicit EdgeToEdge enable

--- android/app/src/main/java/org/welcometomygarden/app2/MainActivity.java
+++ android/app/src/main/java/org/welcometomygarden/app2/MainActivity.java
@@ -8,8 +8,5 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-      // Enable edge-to-edge mode
-      // See https://github.com/capacitor-community/safe-area?tab=readme-ov-file#usage
-      EdgeToEdge.enable(this);
     }
 }
