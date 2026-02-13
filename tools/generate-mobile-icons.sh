20499ab4d24d6f3ff31c53d33f96d612d449a151
Thor Galle <thorgalle@gmail.com>
2026-02-13 16:30:03 +0100

    fix: native app launcher & splash screenicons
    - generate them based on a new higher-quality source .png of 1024x1024 with a transparent background, and more built-in padding, so that a circle mask does not cause clipped icon elements (previously it used a blurry 512^2 png, with a white background)
    - increase logoSplashScale
    - delete dark-mode related icons and splash screen assets. Using them makes no sense, since our app itself does not support dark mode.
    - update the generation script to delete generated dark-mode assets
    - delete unused splash-2732*... sample Ionic assets

--- tools/generate-mobile-icons.sh
+++ tools/generate-mobile-icons.sh
@@ -1,6 +1,19 @@
 # See https://github.com/ionic-team/capacitor-assets
-# skips ios icon generation
+TOOLS_DIR=$(dirname $(realpath $0))
+# This should be run from the root, so the binary can find the iOS & Android projects
+cd $TOOLS_DIR/..
 npx @capacitor/assets generate \
-  --assetPath $(dirname $0)/assets \
+  --assetPath tools/assets \
   --ios --android \
-  --logoSplashTargetWidth 256
+  --logoSplashScale 0.4
+# Remove dark mode assets, to not have half-dark splash screens
+# see https://www.notion.so/slowby/Dark-mode-icons-splash-screens-for-native-3064f49e318e80fd8dcac8ef9289ec7f
+rm -r \
+  android/app/src/main/res/mipmap-night-* \
+  android/app/src/main/res/drawable-*-night-* \
+  ios/App/App/Assets.xcassets/Splash.imageset/Default@*-dark.png
+# Remove references to dark assets
+cd ios/App/App/Assets.xcassets/Splash.imageset
+jq '.images |= map(select(has("appearances") | not))' Contents.json > tmp.json && mv tmp.json Contents.json
+cd - && cd -
+
