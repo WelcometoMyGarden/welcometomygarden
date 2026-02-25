f95a7832b14203cd0fa719827513c44870ca9f05
Thor Galle <thorgalle@gmail.com>
2026-02-25 19:31:59 +0530

    docs: improve android local dev server instructions

--- tools/install-android-hosts.sh
+++ tools/install-android-hosts.sh
@@ -1,12 +1,18 @@
-# Add an appropriate modified /etc/hosts file to this folder
+# Add an appropriate modified /etc/hosts file to this folder (named 'hosts')
+#
 # The sleep steps here are not sufficient, but this does outline a process
 # that should work to update the hosts file in the emulator.
 #
+# Find the base hosts file in Android Studio -> Device Explorer -> /etc/hosts
+# Step 1: start using
+ROOT_PATH=$(dirname $0)
+./run-android-emu.sh
+adb root
+sleep 5
 adb root
 sleep 5
 adb remount
 sleep 5
-ROOT_PATH=$(dirname $0)
 # Update the local IPv4 in the hostsfile
 $ROOT_PATH/setip.sh $ROOT_PATH/hosts
 adb push $ROOT_PATH/hosts /etc/hosts
