97b9926ce945b4d49800484c1de338348e27dae1
Thor Galle <thorgalle@gmail.com>
2026-04-06 12:10:41 +0530

    feat: make ViteSVG more capable
    - fix: use $effect instead of onMount to support a changing icon prop at runtime
    - fix: support potential Vite dev server ?t= query params
    - feat: add a configurable loading spinner while fetching a non-inlined SVG

--- src/lib/components/UI/ViteSVG.svelte
+++ src/lib/components/UI/ViteSVG.svelte
@@ -8,26 +8,61 @@
   so that CSS can target the (inner) SVG elements, <img> does not allow this.
  -->
 <script lang="ts">
-  import { onMount } from 'svelte';
+  import Spinner from './Spinner.svelte';
 
-  type Props = { icon: string | Object[] };
-  let { icon }: Props = $props();
+  type Props = {
+    icon: string | Object[];
+    placeholderWidth?: string;
+    placeholderHeight?: string;
+    placeholderColor?: string;
+  };
+  let {
+    icon,
+    placeholderWidth = '100px',
+    placeholderHeight = '100px',
+    placeholderColor
+  }: Props = $props();
   const SVG_PREAMBLE = 'data:image/svg+xml,';
-  const isInlinedIcon = $derived(typeof icon === 'string' && icon.startsWith(SVG_PREAMBLE));
+  const isInlinedSVG = $derived(typeof icon === 'string' && icon.startsWith(SVG_PREAMBLE));
+  // The icon string ends with .svg and an optional ?t=\d+ query paramter for the Vite dev server
+  const isSVGURL = $derived(typeof icon === 'string' && /\.svg(?:\?t=\d+)?$/.test(icon));
 
-  let svgSource = $state();
-  onMount(async () => {
-    if (typeof icon === 'string' && icon.endsWith('.svg')) {
-      svgSource = await fetch(icon).then((r) => r.text());
+  let isFetchingFromNetwork = $state(false);
+  let fetchedSVGSource = $state();
+
+  $effect(() => {
+    // Note: the icon property might change, therefore, we fetch in $effect
+    // and not in onMount
+    if (isSVGURL && typeof icon === 'string') {
+      isFetchingFromNetwork = true;
+      fetch(icon)
+        .then((r) => r.text())
+        .then((r) => {
+          fetchedSVGSource = r;
+        })
+        .finally(() => (isFetchingFromNetwork = false));
     }
   });
 </script>
 
-{#if isInlinedIcon && typeof icon === 'string'}
+{#if isInlinedSVG && typeof icon === 'string'}
+  <!-- Render the URL-encoded inlined SVG as a <svg> elemeent -->
   {@html decodeURIComponent(icon.replace(SVG_PREAMBLE, ''))}
-{:else if svgSource}
-  {@html svgSource}
+{:else if isFetchingFromNetwork}
+  <!-- Render loading UI -->
+  <div style:height={placeholderHeight} style:width={placeholderWidth}>
+    <Spinner color={placeholderColor} />
+  </div>
+{:else if fetchedSVGSource}
+  <!-- Render the fetched result -->
+  {@html fetchedSVGSource}
 {:else}
-  <!-- ssr: use img as a fallback -->
-  <img src={icon} alt="" />
+  <!-- On SSR and initial mount: use img as a fallback with the icon URL -->
+  <img src={icon as string} alt="" />
 {/if}
+
+<style>
+  div {
+    position: relative;
+  }
+</style>
