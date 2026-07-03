3c05b147db888adfefba7d0353fe718744045501
Thor Galle <thorgalle@gmail.com>
2026-07-03 12:03:38 +0200

    copy: align cookie page with styleguide and add in new languages

--- src/routes/[[lang]]/(static)/terms/cookies/cookie-pages/de.svelte
+++ src/routes/[[lang]]/(static)/terms/cookies/cookie-pages/de.svelte
@@ -11,44 +11,40 @@
   auch einfach als „Cookies“ bezeichnet.
 </p>
 <p>
-  Cookies speichern kleine Informationsstücke auf Ihrem Gerät, sodass unsere Website sie beim
+  Cookies speichern kleine Informationsstücke auf deinem Gerät, sodass unsere Website sie beim
   nächsten Besuch wiedererkennen kann. Diese Seite beschreibt, welche Informationen unsere Cookies
   speichern, warum wir sie benötigen und wie wir sie verwenden.
 </p>
-<p>Wir werden auch erläutern, wie Sie verhindern können, dass diese Cookies gespeichert werden.</p>
+<p>Wir werden auch erläutern, wie du verhindern kannst, dass diese Cookies gespeichert werden.</p>
 <h3>2. Cookies deaktivieren</h3>
 <p>
-  Sie können verhindern, dass Cookies heruntergeladen werden, indem Sie Ihre Browsereinstellungen
-  anpassen (siehe das Hilfemenü Ihres Browsers, um herauszufinden, wie dies funktioniert). Bitte
-  beachten Sie, dass das Deaktivieren von Cookies die Funktionalität dieser und vieler anderer
-  Websites, die Sie besuchen, beeinträchtigen könnte. Das Deaktivieren von Cookies auf unserer
-  Website wird zum Beispiel die Möglichkeit deaktivieren, sich anzumelden.
+  Du kannst verhindern, dass Cookies heruntergeladen werden, indem du deine Browsereinstellungen
+  anpasst (sieh im Hilfemenü deines Browsers nach, um herauszufinden, wie das geht). Bitte beachte,
+  dass das Deaktivieren von Cookies die Funktionalität dieser und vieler anderer Websites, die du
+  besuchst, beeinträchtigen kann. Das Deaktivieren von Cookies auf unserer Website wird zum Beispiel
+  die Möglichkeit deaktivieren, dich anzumelden.
 </p>
 <p>
-  Wenn Sie sich um Ihre Online-Privatsphäre sorgen, empfehlen wir die Verwendung einer
+  Wenn du dir Sorgen um deine Online-Privatsphäre machst, empfehlen wir die Verwendung einer
   Browsererweiterung wie
-  <a
-    href="https://www.google.com/url?q=https://addons.mozilla.org/en-US/firefox/addon/privacy-badger17/&amp;sa=D&amp;ust=1592214262287000"
-    >Privacy Badger</a
-  >
+  <a href="https://addons.mozilla.org/en-US/firefox/addon/privacy-badger17/">Privacy Badger</a>
   (Firefox), <a href="https://www.ghostery.com/ghostery-ad-blocker">Ghostery</a> (viele Browser)
   oder
   <a href="https://ublockorigin.com/">uBlock Origin</a> (viele Browser).
 </p>
 <h3>3. Die von uns gesetzten Cookies</h3>
 <p>
-  Wenn Sie ein neuer Besucher auf unserer Website sind, versuchen wir, nur die Cookies zu setzen,
-  die unbedingt erforderlich sind, um die Dienste von <span class="notranslate"
+  Wenn du ein neuer Besucher auf unserer Website bist, versuchen wir, nur die Cookies zu setzen, die
+  unbedingt erforderlich sind, um die Dienste von <span class="notranslate"
     >Welcome To My Garden</span
-  > bereitzustellen. Wenn möglich, verwenden wir Sitzungscookies: Cookies, die automatisch ablaufen,
-  nachdem Sie Ihren Browser geschlossen haben.
+  > bereitzustellen. Wenn möglich, verwenden wir Sitzungscookies: Cookies, die automatisch ablaufen, nachdem
+  du deinen Browser geschlossen hast.
 </p>
 <p>
   <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies">HTTP-Cookies</a>
   sind der Hauptspeichermechanismus für Cookies. Wir setzen die folgenden HTTP-Cookies als Reaktion auf
-  bestimmte Aktionen, die Sie ausführen:
+  bestimmte Aktionen, die du ausführst:
 </p>
-Here is the translation of the provided content into German:
 <table>
   <thead>
     <tr>
@@ -61,20 +57,19 @@ Here is the translation of the provided content into German:
   <tbody>
     <tr>
       <td>car-notice-dismissed</td>
-      <td>Wenn Sie die Warnung gegen Autofahrten auf unserer Karte wegklicken.</td>
+      <td>Wenn du die Warnung gegen Autofahrten auf unserer Karte wegklickst.</td>
       <td>Um zu verhindern, dass die Benachrichtigung erneut angezeigt wird.</td>
       <td>1 Jahr</td>
     </tr>
     <tr>
       <td>superfan-dismissed</td>
-      <td>Wenn Sie die Benachrichtigung des Superfan-Programms auf unserer Karte wegklicken.</td>
+      <td>Wenn du die Benachrichtigung des Superfan-Programms auf unserer Karte wegklickst.</td>
       <td>Um zu verhindern, dass die Benachrichtigung erneut angezeigt wird.</td>
       <td>1 Monat</td>
     </tr>
     <tr>
       <td>lang</td>
-      <td>Wenn Sie angeben, dass Sie eine andere Sprache als die Sprache Ihres Browsers möchten.</td
-      >
+      <td>Wenn du angibst, dass du eine andere Sprache als die deines Browsers möchtest.</td>
       <td>Um die Website in der gewünschten Sprache bereitzustellen.</td>
       <td>Unbefristet</td>
     </tr>
@@ -85,7 +80,7 @@ Here is the translation of the provided content into German:
     href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API"
     >Web Storage</a
   >
-  ist eine alternative Technologie, um Informationen in Ihrem Browser zu speichern. Wir setzen diese
+  ist eine alternative Technologie, um Informationen in deinem Browser zu speichern. Wir setzen diese
   Informationen automatisch:
 </p>
 <table>
@@ -100,23 +95,23 @@ Here is the translation of the provided content into German:
   <tbody>
     <tr>
       <td>mapbox.eventData.uuid</td>
-      <td>Wenn Sie die WTMG-Karte mit Gärten laden.</td>
+      <td>Wenn du die WTMG-Karte mit Gärten lädst.</td>
       <td>Sitzung</td>
       <td>
         Unser wesentlicher Kartenservice-Anbieter https://www.mapbox.com/ speichert diese anonyme
-        Benutzerkennung des Browsers und sendet sie an die Mapbox-Server, wenn Sie eine Karte auf
-        unserer Website laden. Unser eigener Webanwendungscode löscht alle verbleibenden Kennungen
-        dieser Art, wenn Sie unsere Website laden, was sie im Wesentlichen zu einem Sitzungscookie
+        Benutzerkennung des Browsers und sendet sie an die Mapbox-Server, wenn du eine Karte auf
+        unserer Website lädst. Unser eigener Webanwendungscode löscht alle verbleibenden Kennungen
+        dieser Art, wenn du unsere Website lädst, was sie im Wesentlichen zu einem Sitzungscookie
         macht. Wir haben bei Mapbox nach dem Zweck dieser Kennung gefragt.
       </td>
     </tr>
     <tr>
       <td>sveltekit:scroll</td>
-      <td>Wenn Sie zu einer anderen Seite navigieren.</td>
+      <td>Wenn du zu einer anderen Seite navigierst.</td>
       <td>Sitzung</td>
       <td>
-        Wird verwendet, um die alte Scroll-Position der Seite zu speichern, wenn Sie zur vorherigen
-        Seite in Ihrem Browser zurückkehren. Dies wird niemals an einen Webserver gesendet.
+        Wird verwendet, um die alte Scroll-Position der Seite zu speichern, wenn du zur vorherigen
+        Seite in deinem Browser zurückkehrst. Dies wird niemals an einen Webserver gesendet.
         <a
           href="https://github.com/sveltejs/kit/blob/36b0168dfa487ad27c223090345e7d8d0ddaefff/packages/kit/src/runtime/client/client.js#L37-L40."
           >Mehr Informationen auf dieser Webseite.</a
@@ -146,35 +141,36 @@ Here is the translation of the provided content into German:
   Wir verwenden <a href="https://plausible.io/">Plausible</a>, ein datenschutzorientiertes
   Analysetool, um anonym zu verstehen, wie unsere Besucher
   <span class="notranslate">Welcome To My Garden</span> nutzen. Plausible ermöglicht es uns, die Anzahl
-  der Besucher auf unserer Website zu zählen, zu verstehen, welche Seiten am meisten besucht werden,
-  woher sie kommen und welche Aktionen die Benutzer auf unserer Seite ausführen.
+  der Besucher auf unserer Website zu zählen, zu verstehen, welche Seiten am meisten besucht werden, woher
+  sie kommen und welche Aktionen die Benutzer auf unserer Seite ausführen.
 </p>
 <p>
   Im Gegensatz zu vielen anderen Analyselösungen speichert Plausible
   <a href="https://plausible.io/privacy-focused-web-analytics">keine <em>einzigen</em> Cookies</a>
-  oder Website-Daten in Ihrem Browser. Das bedeutet, dass wir oder unser Analysedienstleister
+  oder Website-Daten in deinem Browser. Das bedeutet, dass wir oder unser Analysedienstleister
   <strong><em>nicht</em>:</strong>
 </p>
 <ul>
-  <li>Sie über verschiedene Websites hinweg verfolgen können</li>
-  <li>Sie über verschiedene Geräte hinweg verfolgen können</li>
+  <li>dich über verschiedene Websites hinweg verfolgen können</li>
+  <li>dich über verschiedene Geräte hinweg verfolgen können</li>
   <li>
-    Sie über mehrere Tage hinweg verfolgen können (wir wissen nicht, ob Sie morgen zurückkehren!).
+    dich über mehrere Tage hinweg verfolgen können (wir wissen nicht, ob du morgen zurückkehrst!).
   </li>
 </ul>
 <p>
   Um mehr über die datenschutzfreundliche Art und Weise zu erfahren, wie Plausible Statistiken
-  sammelt, lesen Sie
+  sammelt, lies
   <a href="https://plausible.io/data-policy">deren Datenrichtlinie</a>.
 </p>
 <h3>5. Weitere Informationen</h3>
 <p>
-  Hoffentlich hat diese Seite Ihnen die Dinge klargestellt. Wie bereits erwähnt, wenn Sie sich nicht
-  sicher sind, ob Sie etwas benötigen oder nicht, ist es in der Regel sicherer, Cookies aktiviert zu
-  lassen - nur für den Fall, dass sie mit einer der Funktionen interagieren, die Sie auf unserer
-  Website verwenden. Wenn Sie immer noch nach weiteren Informationen suchen, können Sie uns unter <a
+  Hoffentlich hat diese Seite dir die Dinge klargestellt. Wie bereits erwähnt: Wenn du dir bei etwas
+  nicht sicher bist, ob du es brauchst oder nicht, ist es in der Regel sicherer, Cookies aktiviert
+  zu lassen - nur für den Fall, dass sie mit einer der Funktionen interagieren, die du auf unserer
+  Website verwendest. Wenn du weiterhin nach weiteren Informationen suchst, kannst du uns unter <a
     href="mailto:support@welcometomygarden.org">support@welcometomygarden.org</a
-  > kontaktieren.
+  >
+  kontaktieren.
 </p>
 
 <style src="../style.css"></style>
---
+++ src/routes/[[lang]]/(static)/terms/cookies/cookie-pages/sv.svelte
@@ -0,0 +1,170 @@
+<script lang="ts">
+  import routes from '$lib/routes';
+  import { lr } from '$lib/util/translation-helpers';
+</script>
+
+<h2>Cookiepolicy</h2>
+<h3>1. Vad är cookies</h3>
+<p>
+  Som är vanlig praxis på nästan alla professionella webbplatser använder den här sidan cookies och
+  andra tekniker för lagring av webbplatsdata, till exempel Web Storage och IndexedDB, hädanefter
+  även kallade enbart ”cookies”.
+</p>
+<p>
+  Cookies lagrar små informationsbitar på din enhet, så att vår webbplats kan komma ihåg dem nästa
+  gång du besöker den. Den här sidan beskriver vilken information våra cookies lagrar, varför vi
+  behöver dem och hur vi använder dem.
+</p>
+<p>Vi kommer också att förklara hur du kan förhindra att dessa cookies lagras.</p>
+<h3>2. Inaktivera cookies</h3>
+<p>
+  Du kan förhindra att cookies laddas ner genom att justera dina webbläsarinställningar (se
+  hjälpmenyn i din webbläsare för att ta reda på hur du gör det här). Tänk på att inaktivering av
+  cookies kan påverka funktionaliteten hos den här och många andra webbplatser som du besöker. Att
+  inaktivera cookies för vår webbplats i synnerhet kommer till exempel att inaktivera möjligheten
+  att logga in.
+</p>
+<p>
+  Om du är orolig för din integritet online rekommenderar vi att du använder ett webbläsartillägg
+  som <a href="https://addons.mozilla.org/en-US/firefox/addon/privacy-badger17/">Privacy Badger</a>
+  (Firefox), <a href="https://www.ghostery.com/ghostery-ad-blocker">Ghostery</a> (många webbläsare)
+  eller <a href="https://ublockorigin.com/">uBlock Origin</a> (många webbläsare).
+</p>
+<h3>3. De cookies vi sätter</h3>
+<p>
+  När du är en ny besökare på vår webbplats försöker vi bara sätta de cookies som är absolut
+  nödvändiga för att tillhandahålla tjänsterna från <span class="notranslate"
+    >Welcome To My Garden</span
+  >. När det är möjligt använder vi sessionscookies: cookies som upphör att gälla automatiskt när du
+  stänger din webbläsare.
+</p>
+<p>
+  <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies">HTTP-cookies</a>
+  är den huvudsakliga lagringsmekanismen för cookies. Vi sätter följande HTTP-cookies som svar på vissa
+  åtgärder du vidtar:
+</p>
+<table>
+  <thead>
+    <tr>
+      <th>Cookie-namn</th>
+      <th>När sätts den?</th>
+      <th>Syfte</th>
+      <th>Varaktighet</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>car-notice-dismissed</td>
+      <td>När du klickar bort varningen mot bilresor på vår karta.</td>
+      <td>För att förhindra att meddelandet visas igen.</td>
+      <td>1 år</td>
+    </tr>
+    <tr>
+      <td>superfan-dismissed</td>
+      <td>När du klickar bort meddelandet om Superfan-programmet på vår karta.</td>
+      <td>För att förhindra att meddelandet visas igen.</td>
+      <td>1 månad</td>
+    </tr>
+    <tr>
+      <td>lang</td>
+      <td>När du anger att du vill ha ett annat webbplatsspråk än ditt webbläsarspråk.</td>
+      <td>För att tillhandahålla webbplatsen på det språk du begärt.</td>
+      <td>Obestämd tid</td>
+    </tr>
+  </tbody>
+</table>
+<p>
+  <a
+    href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API"
+    >Web storage</a
+  >
+  är en alternativ teknik för att lagra information i din webbläsare. Vi sätter dessa informationsbitar
+  automatiskt:
+</p>
+<table>
+  <thead>
+    <tr>
+      <th>Namn</th>
+      <th>När sätts den?</th>
+      <th>Hur länge finns den kvar?</th>
+      <th>Syfte</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>mapbox.eventData.uuid</td>
+      <td>När du laddar WTMG-kartan med trädgårdar.</td>
+      <td>Session</td>
+      <td>
+        Vår viktiga kartleverantör https://www.mapbox.com/ lagrar denna anonyma
+        webbläsaridentifierare och skickar den till Mapbox-servrar när du laddar en karta på vår
+        webbplats. Vår egen webbapplikationskod rensar alla återstående identifierare av den här
+        typen när du laddar vår webbplats, vilket i praktiken gör den till en sessionscookie. Vi har
+        frågat Mapbox om syftet med denna identifierare.
+      </td>
+    </tr>
+    <tr>
+      <td>sveltekit:scroll</td>
+      <td>När du navigerar till en annan sida.</td>
+      <td>Session</td>
+      <td>
+        Används för att komma ihåg sidans tidigare rullningsposition om du navigerar till föregående
+        sida i din webbläsare. Detta skickas aldrig till någon webbserver.
+        <a
+          href="https://github.com/sveltejs/kit/blob/36b0168dfa487ad27c223090345e7d8d0ddaefff/packages/kit/src/runtime/client/client.js#L37-L40."
+          >Mer information på den här webbsidan.</a
+        >
+      </td>
+    </tr>
+  </tbody>
+</table>
+<p>
+  Om du skapar ett konto hos oss, eller använder vår karta, kommer vår tjänsteleverantör Firebase
+  att använda webbplatsdata som lokal lagring eller IndexedDB för hanteringen av
+  registreringsprocessen, för att hålla dig inloggad och för att säkerställa att applikationen
+  fungerar korrekt. Firebase har en sida som beskriver
+  <a
+    href="https://firebase.google.com/support/privacy/#examples-end-user-data-processed-by-firebase"
+    >vilken typ av data den lagrar</a
+  >. Tänk på att vi från Firebase endast använder tjänsterna &quot;Cloud Functions for
+  Firebase&quot;, &quot;Firebase Authentication&quot; och &quot;Cloud Firestore&quot;. Vi har
+  inaktiverat, eller använder inte, alla andra tjänster.
+</p>
+<p>
+  För att lära dig mer om den information vi samlar in när du skapar ett konto, läs vår
+  <a href={$lr(routes.PRIVACY_POLICY)}>integritetspolicy</a>.
+</p>
+<h3>4. Integritetsvänlig analys</h3>
+<p>
+  Vi använder <a href="https://plausible.io/">Plausible</a>, ett integritetsfokuserat analysverktyg,
+  för att anonymt förstå hur våra besökare använder
+  <span class="notranslate">Welcome To My Garden</span>. Plausible låter oss räkna antalet besökare
+  på vår webbplats, förstå vilka sidor som besöks mest, varifrån de kommer och vilka åtgärder
+  människor vidtar på vår webbplats.
+</p>
+<p>
+  Till skillnad från många andra analyslösningar lagrar Plausible
+  <a href="https://plausible.io/privacy-focused-web-analytics">inga <em>som helst</em> cookies</a>
+  eller webbplatsdata i din webbläsare. Det betyder att vi, eller vår analystjänsteleverantör,
+  <strong>inte <em>kan</em>:</strong>
+</p>
+<ul>
+  <li>spåra dig på olika webbplatser</li>
+  <li>spåra dig på olika enheter</li>
+  <li>spåra dig över flera dagar (vi vet inte om du kommer tillbaka imorgon!).</li>
+</ul>
+<p>
+  För att lära dig mer om det integritetsvänliga sätt som Plausible samlar in statistik på, läs
+  <a href="https://plausible.io/data-policy">deras datapolicy</a>.
+</p>
+<h3>5. Mer information</h3>
+<p>
+  Förhoppningsvis har den här sidan klargjort saker för dig. Som tidigare nämnts, om det är något du
+  är osäker på om du behöver eller inte, är det oftast säkrare att lämna cookies aktiverade - ifall
+  det skulle interagera med någon av funktionerna du använder på vår webbplats. Om du fortfarande
+  letar efter mer information kan du kontakta oss på <a href="mailto:support@welcometomygarden.org"
+    >support@welcometomygarden.org</a
+  >.
+</p>
+
+<style src="../style.css"></style>
